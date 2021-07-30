01b368ecd143200621399835ed88a797ce9e7486
markgray <mark.t.gray@gmail.com>
2021-07-30 06:14:32 -0400

    Added comments to DirectoryEntryAdapter.kt in ActionOpenDocumentTree

--- ActionOpenDocumentTree/app/src/main/java/com/example/android/ktfiles/DirectoryEntryAdapter.kt
+++ ActionOpenDocumentTree/app/src/main/java/com/example/android/ktfiles/DirectoryEntryAdapter.kt
@@ -16,6 +16,8 @@
 
 package com.example.android.ktfiles
 
+import android.annotation.SuppressLint
+import android.content.Context
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -34,18 +36,70 @@ class DirectoryEntryAdapter(
     private val clickListeners: ClickListeners
 ) : RecyclerView.Adapter<DirectoryEntryAdapter.ViewHolder>() {
 
+    /**
+     * Our dataset of [CachingDocumentFile] objects.
+     */
     private val directoryEntries = mutableListOf<CachingDocumentFile>()
 
+    /**
+     * Called when RecyclerView needs a new [ViewHolder] of the given type to represent an item.
+     * This new [ViewHolder] should be constructed with a new [View] that can represent the items
+     * of the given type. You can either create a new [View] manually or inflate it from an XML
+     * layout file.
+     *
+     * The new [ViewHolder] will be used to display items of the adapter using [onBindViewHolder].
+     * Since it will be re-used to display different items in the data set, it is a good idea to
+     * cache references to sub views of the View to avoid unnecessary [View.findViewById] calls.
+     *
+     * We initialize our [View] variable `val view` by retrieving the [LayoutInflater] from the
+     * [Context] of our [ViewGroup] parameter [parent] and using it to inflate the layout file with
+     * ID [R.layout.directory_item] using [parent] for its LayoutParams without attaching to it.
+     * Finally we return a new instance of [ViewHolder] constructed to hold `view`.
+     *
+     * @param parent   The [ViewGroup] into which the new [View] will be added after it is bound to
+     * an adapter position.
+     * @param viewType The view type of the new [View].
+     * @return A new [ViewHolder] that holds a [View] of the given view type.
+     */
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
         val view = LayoutInflater.from(parent.context)
             .inflate(R.layout.directory_item, parent, false)
         return ViewHolder(view)
     }
 
+    /**
+     * Called by [RecyclerView] to display the data at the specified position. This method should
+     * update the contents of the [ViewHolder.itemView] to reflect the item at the given position.
+     *
+     * We use the kotlin [with] method to call a lambda function block using our [ViewHolder] parameter
+     * [viewHolder] as the receiver and in this lambda we:
+     *  - Initialize our [CachingDocumentFile] variable `val item` to the object in position [position]
+     *  in our [MutableList] of [CachingDocumentFile] dataset field [directoryEntries].
+     *  - Initialize our [Int] variable `val itemDrawableRes` to the drawable resource ID
+     *  [R.drawable.ic_folder_black_24dp] if `item` is a directory, or to [R.drawable.ic_file_black_24dp]
+     *  if it is not a directory.
+     *  - Set the text of the [TextView] field [ViewHolder.fileName] of our receiver to the
+     *  [CachingDocumentFile.name] field of `item`.
+     *  - Set the text of the [TextView] field [ViewHolder.mimeType] of our receiver to the
+     *  [CachingDocumentFile.type] field of `item` or to the empty [String] if it is `null`.
+     *  - Set the content of the [ImageView] field [ViewHolder.imageView] of our receiver to the
+     *  drawable with resource ID `itemDrawableRes`.
+     *  - Set the [View.OnClickListener] of the [View] field [ViewHolder.root] of our receiver to
+     *  to a lambda that calls the [ClickListeners.onDocumentClicked] override of our [clickListeners]
+     *  field with `item` as its parameter.
+     *  - Set the [View.OnLongClickListener] of the [View] field [ViewHolder.root] of our receiver
+     *  to a lambda that calls the [ClickListeners.onDocumentLongClicked] override of our
+     *  [clickListeners] field with `item` as its parameter, and returns `true` to the caller to
+     *  consume the long click.
+     *
+     * @param viewHolder The [ViewHolder] which should be updated to represent the contents of the
+     * item at the given position in the data set.
+     * @param position The position of the item within the adapter's data set.
+     */
     override fun onBindViewHolder(viewHolder: ViewHolder, position: Int) {
         with(viewHolder) {
-            val item = directoryEntries[position]
-            val itemDrawableRes = if (item.isDirectory) {
+            val item: CachingDocumentFile = directoryEntries[position]
+            val itemDrawableRes: Int = if (item.isDirectory) {
                 R.drawable.ic_folder_black_24dp
             } else {
                 R.drawable.ic_file_black_24dp
@@ -65,8 +119,15 @@ class DirectoryEntryAdapter(
         }
     }
 
+    /**
+     * Returns the total number of items in the data set held by the adapter. We just return the
+     * size of our [MutableList] of [CachingDocumentFile] dataset field [directoryEntries].
+     *
+     * @return The total number of items in this adapter.
+     */
     override fun getItemCount() = directoryEntries.size
 
+    @SuppressLint("NotifyDataSetChanged")  // TODO: Move to DiffUtil
     fun setEntries(newList: List<CachingDocumentFile>) {
         synchronized(directoryEntries) {
             directoryEntries.clear()
