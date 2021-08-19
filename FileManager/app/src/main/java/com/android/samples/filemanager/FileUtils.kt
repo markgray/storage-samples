d1bcd19cbd98f0ceb1dd25686fefdc057b9cb3c3
markgray <mark.t.gray@gmail.com>
2021-08-19 06:07:34 -0400

    Added comments to FileUtils.kt in FileManager

--- FileManager/app/src/main/java/com/android/samples/filemanager/FileUtils.kt
+++ FileManager/app/src/main/java/com/android/samples/filemanager/FileUtils.kt
@@ -17,14 +17,27 @@
 package com.android.samples.filemanager
 
 import android.content.Intent
+import android.net.Uri
 import android.os.Environment
 import android.webkit.MimeTypeMap
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.content.FileProvider
 import java.io.File
 
+/**
+ * The authority of the [FileProvider] we use to share files with other apps. A `provider` element
+ * in the `application` element of our AndroidManifest declares a content provider component using
+ * this authority, and we use [AUTHORITY] to build the [Uri] for the [File] we are to share.
+ */
 private const val AUTHORITY = "${BuildConfig.APPLICATION_ID}.provider"
 
+/**
+ * Returns the mime type of its [String] parameter [url], where [url] is the string value of the
+ * content [Uri] for a [File].
+ *
+ * @param url the string value of the content [Uri] for a [File] whose mime type we need to know.
+ * @return the [String] representing the MIME type deduced from the extension of the [File].
+ */
 fun getMimeType(url: String): String {
     val ext = MimeTypeMap.getFileExtensionFromUrl(url)
     return MimeTypeMap.getSingleton().getMimeTypeFromExtension(ext) ?: "text/plain"
@@ -56,7 +69,7 @@ fun renderItem(activity: AppCompatActivity, file: File): String {
 
 fun openFile(activity: AppCompatActivity, selectedItem: File) {
     // Get URI and MIME type of file
-    val uri = FileProvider.getUriForFile(activity.applicationContext, AUTHORITY, selectedItem)
+    val uri: Uri = FileProvider.getUriForFile(activity.applicationContext, AUTHORITY, selectedItem)
     val mime: String = getMimeType(uri.toString())
 
     // Open file with user selected app
