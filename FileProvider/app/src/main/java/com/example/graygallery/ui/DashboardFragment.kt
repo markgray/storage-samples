0e9e162f27fa29cfecf2908556a6939a73215280
markgray <mark.t.gray@gmail.com>
2021-08-31 06:03:09 -0400

    Added comments to DashboardFragment.kt in FileProvider

--- FileProvider/app/src/main/java/com/example/graygallery/ui/DashboardFragment.kt
+++ FileProvider/app/src/main/java/com/example/graygallery/ui/DashboardFragment.kt
@@ -29,16 +29,38 @@ import androidx.activity.result.contract.ActivityResultContract
 import androidx.activity.result.contract.ActivityResultContracts.TakePicturePreview
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
+import androidx.viewbinding.ViewBinding
 import com.example.graygallery.databinding.FragmentDashboardBinding
 import com.google.android.material.snackbar.Snackbar
 
+/**
+ * This is the start destination of our demo, and consists of four buttons:
+ *  - "Take picture" takes a picture using the phones camera and stores it in our private storage area
+ *  - "Select picture" allows the user to select a picture from a variety of sources ("Google Drive".
+ *  "Google Photos", from the phone itself etc.) and stores it in our private storage area.
+ *  - "Add Unsplash random image" downloads a random image from the "Unsplash" website and stores it
+ *  in our private storage area.
+ *  - "Clear files" removes all the files in the "images/" directory in our private storage area
+ *  and deletes the "images/" directory as well.
+ */
 class DashboardFragment : Fragment() {
+    /**
+     * The [AppViewModel] used by both [DashboardFragment] and [GalleryFragment].
+     */
     private val viewModel by viewModels<AppViewModel>()
+
+    /**
+     * The [ViewBinding] which is inflated from our layout file layout/fragment_dashboard.xml, it
+     * consists of a `ConstraintLayout` root view holding a vertical `LinearLayout` which holds
+     * four buttons labeled: "Take picture", "Select picture", "Add Unsplash random image", and
+     * "Clear files".
+     */
     private lateinit var binding: FragmentDashboardBinding
 
-    private val takePicture = registerForActivityResult(TakePicturePreview()) { bitmap ->
-        viewModel.saveImageFromCamera(bitmap)
-    }
+    private val takePicture: ActivityResultLauncher<Void> =
+        registerForActivityResult(TakePicturePreview()) { bitmap ->
+            viewModel.saveImageFromCamera(bitmap)
+        }
 
     private val selectPicture: ActivityResultLauncher<Array<String>> =
         registerForActivityResult(GetContentWithMimeTypes()) { uri ->
