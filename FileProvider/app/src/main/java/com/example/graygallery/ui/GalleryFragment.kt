43b5e18cd4e3777948a2b518a5071b0a9dd0195b
markgray <mark.t.gray@gmail.com>
2021-09-03 07:43:21 -0400

    Added comments to GalleryFragment.kt in FileProvider

--- FileProvider/app/src/main/java/com/example/graygallery/ui/GalleryFragment.kt
+++ FileProvider/app/src/main/java/com/example/graygallery/ui/GalleryFragment.kt
@@ -26,16 +26,45 @@ import androidx.core.content.FileProvider
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import androidx.viewbinding.ViewBinding
 import com.example.graygallery.databinding.FragmentGalleryBinding
 import com.google.android.material.snackbar.Snackbar
 import java.io.File
 
+/**
+ * Number of columns in the grid of our [RecyclerView].
+ */
 const val GALLERY_COLUMNS = 3
 
+/**
+ * This [Fragment] displays all of the images we have downloaded to our "images/" folder.
+ */
 class GalleryFragment : Fragment() {
+    /**
+     * The [AppViewModel] used by both [DashboardFragment] and [GalleryFragment].
+     */
     private val viewModel by viewModels<AppViewModel>()
+    /**
+     * The [ViewBinding] which is inflated from our layout file layout/fragment_gallery.xml, it
+     * consists of a `ConstraintLayout` root view holding a [RecyclerView].
+     */
     private lateinit var binding: FragmentGalleryBinding
 
+    /**
+     * Called to have the fragment instantiate its user interface view. This will be called between
+     * [onCreate] and [onViewCreated]. It is recommended to only inflate the layout in this method
+     * and move logic that operates on the returned [View] to [onViewCreated].
+     *
+     * @param inflater The [LayoutInflater] object that can be used to inflate
+     * any views in the fragment,
+     * @param container If non-`null`, this is the parent view that the fragment's
+     * UI will be attached to. The fragment should not add the view itself,
+     * but this can be used to generate the `LayoutParams` of the view.
+     * @param savedInstanceState If non-`null`, this fragment is being re-constructed
+     * from a previous saved state as given here.
+     * @return Return the [View] for the fragment's UI, or null.
+     */
     @Suppress("RedundantNullableReturnType")
     override fun onCreateView(
         inflater: LayoutInflater,
