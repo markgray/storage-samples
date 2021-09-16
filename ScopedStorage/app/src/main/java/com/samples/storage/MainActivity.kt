36595d31c1c824708fc2e37d16b9e919251df1b0
markgray <mark.t.gray@gmail.com>
2021-09-16 06:13:47 -0400

    Added comments to MainActivity.kt in ScopedStorage

--- ScopedStorage/app/src/main/java/com/samples/storage/MainActivity.kt
+++ ScopedStorage/app/src/main/java/com/samples/storage/MainActivity.kt
@@ -17,26 +17,48 @@ package com.samples.storage
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.NavController
 import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.navigateUp
 import androidx.navigation.ui.setupActionBarWithNavController
 
+/**
+ * This class just sets the content view to the layout file [R.layout.main_activity] and configures
+ * the [NavController] of the [NavHostFragment] in that file (resource ID [R.id.nav_host_fragment])
+ * to be used by the `ActionBar` for "UP" button behavior. The layout file consists of a root
+ * `ConstraintLayout` holding only a `fragment` container whose contents is the [NavHostFragment].
+ * The "app:navGraph" element specifies the file navigation/nav_graph.xml to be the navigation graph
+ * for the [NavHostFragment] and [MainFragment] is the start fragment of the graph.
+ */
 class MainActivity : AppCompatActivity() {
+    /**
+     * Configuration options for the action bar `NavigationUI` methods (such as "UP"). We construct
+     * this instance in our [onCreate] override to use the navigation graph associated with the
+     * [NavController] for the [NavHostFragment] in the [R.id.nav_host_fragment] `fragment` of our
+     * layout file. Its start destination ([MainFragment]) will be considered the only top level
+     * destination. The Up button will not be displayed when on the start destination of the graph.
+     */
     private lateinit var appBarConfiguration: AppBarConfiguration
 
+    /**
+     * Called then the activity is starting.
+     *
+     * @param savedInstanceState we do not override [onSaveInstanceState] so do not use.
+     */
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.main_activity)
 
-        val navController = findNavController(R.id.nav_host_fragment)
+        val navController: NavController = findNavController(R.id.nav_host_fragment)
 
         appBarConfiguration = AppBarConfiguration(navController.graph)
         setupActionBarWithNavController(navController, appBarConfiguration)
     }
 
     override fun onSupportNavigateUp(): Boolean {
-        val navController = findNavController(R.id.nav_host_fragment)
+        val navController: NavController = findNavController(R.id.nav_host_fragment)
         return navController.navigateUp(appBarConfiguration) ||
             super.onSupportNavigateUp()
     }
