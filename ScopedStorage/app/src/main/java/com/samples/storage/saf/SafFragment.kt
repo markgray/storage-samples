a17b501049ef92ba84a0d9455f2de61445f56dfd
markgray <mark.t.gray@gmail.com>
2021-09-30 06:53:41 -0400

    Added comments to SafFragment.kt in ScopedStorage

--- ScopedStorage/app/src/main/java/com/samples/storage/saf/SafFragment.kt
+++ ScopedStorage/app/src/main/java/com/samples/storage/saf/SafFragment.kt
@@ -15,6 +15,8 @@
  */
 package com.samples.storage.saf
 
+import android.content.ContentResolver
+import android.content.Context
 import android.content.Intent
 import android.net.Uri
 import android.os.Bundle
@@ -34,12 +36,17 @@ import androidx.documentfile.provider.DocumentFile
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.LifecycleOwner
 import androidx.lifecycle.lifecycleScope
 import com.samples.storage.R
 import com.samples.storage.databinding.FragmentSafBinding
+import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
+import java.io.InputStream
+import java.io.OutputStream
+import kotlin.coroutines.CoroutineContext
 
 private const val DEFAULT_FILE_NAME = "SAF Demo File.txt"
 
@@ -79,28 +86,50 @@ class SafFragment : Fragment() {
      * which is used as the initial default file name (which can be changed by the user), and returns
      * the [Uri] that the [DocumentsProvider] returns in the [Intent] via its [Intent.getData] (aka
      * kotlin `data` property) to the lambda argument of [registerForActivityResult].
+     *
+     * In the lambda argument to [registerForActivityResult] we initialize our [Uri] variable
+     * `val documentUri` to the `uri` argument to the lambda, returning to [registerForActivityResult]
+     * if this is `null` (the user returned to this fragment without creating a file). Otherwise we
+     * initialize our [DocumentFile] variable `val documentFile` to the instance that the
+     * [DocumentFile.fromSingleUri] method creates to represent the single document at the [Uri]
+     * `documentUri` again returning to [registerForActivityResult] if this is `null` (probably
+     * means the file has been removed or became unavailable). We get a [LifecycleOwner] that
+     * represents the [Fragment]'s View lifecycle, and use the [CoroutineScope] tied to this
+     * [LifecycleOwner]'s Lifecycle to launch a new coroutine block. In this block we initialize
+     * our [OutputStream] variable `val documentStream` to the result of the execution of a
+     * suspending block in the [CoroutineContext] of [Dispatchers.IO] with that suspending block
+     * returning the [OutputStream] that the [ContentResolver.openOutputStream] method of a
+     * [ContentResolver] instance for our application's package opens to write to the content
+     * associated with the content [Uri] `documentUri` (exiting the launch suspending block if this
+     * is `null`). If `documentStream` is not `null` the suspending block initializes its [String]
+     * variable `val text` to the value returned by the [SafFragmentViewModel.createDocumentExample]
+     * method of [viewModel] when it writes that same [String] to the file `documentStream`. We then
+     * set the text of the [TextView] found at the [FragmentSafBinding.output] field of [binding]
+     * to a [String] formed by concatenating the string "File:" followed by the name of the file
+     * returned by [DocumentFile.getName] (aka kotlin `name` property) of `documentFile`, followed
+     * by the [String] `text`.
      */
     private val actionCreateDocument: ActivityResultLauncher<String> =
         registerForActivityResult(CreateDocument()) { uri: Uri? ->
             // If the user returns to this fragment without creating a file, uri will be null
             // In this case, we return void
-            val documentUri = uri ?: return@registerForActivityResult
+            val documentUri: Uri = uri ?: return@registerForActivityResult
 
             // If we can't instantiate a `DocumentFile`, it probably means the file has been removed
             // or became unavailable (if the SD card has been removed).
             // In this case, we return void
-            val documentFile = DocumentFile.fromSingleUri(requireContext(), documentUri)
+            val documentFile: DocumentFile = DocumentFile.fromSingleUri(requireContext(), documentUri)
                 ?: return@registerForActivityResult
 
             // We launch a coroutine within the lifecycle of the viewmodel. The coroutine will be
             // automatically cancelled if the viewmodel is cleared
             viewLifecycleOwner.lifecycleScope.launch {
                 @Suppress("BlockingMethodInNonBlockingContext")
-                val documentStream = withContext(Dispatchers.IO) {
+                val documentStream: OutputStream = withContext(Dispatchers.IO) {
                     requireContext().contentResolver.openOutputStream(documentUri)
                 } ?: return@launch
 
-                val text = viewModel.createDocumentExample(documentStream)
+                val text: String = viewModel.createDocumentExample(documentStream)
                 binding.output.text =
                     getString(R.string.saf_create_file_output, documentFile.name, text)
             }
@@ -108,36 +137,92 @@ class SafFragment : Fragment() {
             Log.d("SafFragment", "Created: ${documentFile.name}, type ${documentFile.type}")
         }
 
-    private val actionOpenDocument = registerForActivityResult(OpenDocument()) { uri ->
+    /**
+     * The [ActivityResultLauncher] that the user launches when they click the "Open Document"
+     * [Button]. The [ActivityResultContract] argument [OpenDocument] to [registerForActivityResult]
+     * is an [ActivityResultContract] whose [ActivityResultLauncher.launch] method takes a [String]
+     * array of the mime types to filter by and returns the [Uri] that the [DocumentsProvider] returns
+     * in the [Intent] via its [Intent.getData] (aka kotlin `data` property) to the lambda argument
+     * of [registerForActivityResult].
+     *
+     * In the lambda argument to [registerForActivityResult] we initialize our [Uri] variable
+     * `val documentUri` to the `uri` argument to the lambda, returning to [registerForActivityResult]
+     * if this is `null` (the user returned to this fragment without selecting a file). Otherwise we
+     * initialize our [DocumentFile] variable `val documentFile` to the instance that the
+     * [DocumentFile.fromSingleUri] method creates to represent the single document at the [Uri]
+     * `documentUri` again returning to [registerForActivityResult] if this is `null` (probably
+     * means the file has been removed or became unavailable). We get a [LifecycleOwner] that
+     * represents the [Fragment]'s View lifecycle, and use the [CoroutineScope] tied to this
+     * [LifecycleOwner]'s Lifecycle to launch a new coroutine block. In this block we initialize
+     * our [InputStream] variable `val documentStream` to the result of the execution of a
+     * suspending block in the [CoroutineContext] of [Dispatchers.IO] with that suspending block
+     * returning the [InputStream] that the [ContentResolver.openInputStream] method of a
+     * [ContentResolver] instance for our application's package opens to read the content associated
+     * with the content [Uri] `documentUri` (exiting the launch suspending block if this is `null`).
+     * If `documentStream` is not `null` the suspending block initializes its [String] variable
+     * `val text` to the value returned by the [SafFragmentViewModel.openDocumentExample] method of
+     * [viewModel] when it reads the contents of the [InputStream] `documentStream`, creates a
+     * [String] hashcode of that contents and returns that to us. We then set the text of the
+     * [TextView] found at the [FragmentSafBinding.output] field of [binding] to a [String] formed
+     * by concatenating the string "File:" followed by the name of the file returned by
+     * [DocumentFile.getName] (aka kotlin `name` property) of `documentFile`, followed by the
+     * string "Contents hash:" followed by the [String] `text`.
+     */
+    private val actionOpenDocument = registerForActivityResult(OpenDocument()) { uri: Uri? ->
         // If the user returns to this fragment without selecting a file, uri will be null
         // In this case, we return void
-        val documentUri = uri ?: return@registerForActivityResult
+        val documentUri: Uri = uri ?: return@registerForActivityResult
 
         // If we can't instantiate a `DocumentFile`, it probably means the file has been removed
         // or became unavailable (if the SD card has been removed).
         // In this case, we return void
-        val documentFile = DocumentFile.fromSingleUri(requireContext(), documentUri)
+        val documentFile: DocumentFile = DocumentFile.fromSingleUri(requireContext(), documentUri)
             ?: return@registerForActivityResult
 
         viewLifecycleOwner.lifecycleScope.launch {
             @Suppress("BlockingMethodInNonBlockingContext")
-            val documentStream = withContext(Dispatchers.IO) {
+            val documentStream: InputStream = withContext(Dispatchers.IO) {
                 requireContext().contentResolver.openInputStream(documentUri)
             } ?: return@launch
 
-            val text = viewModel.openDocumentExample(documentStream)
+            val text: String = viewModel.openDocumentExample(documentStream)
             binding.output.text = getString(R.string.saf_open_file_output, documentFile.name, text)
         }
     }
 
+    /**
+     * The [ActivityResultLauncher] that the user launches when they click the "Open Folder"
+     * [Button]. The [ActivityResultContract] argument [OpenDocumentTree] to [registerForActivityResult]
+     * is an [ActivityResultContract] whose [ActivityResultLauncher.launch] method takes an optional
+     * [Uri] of the initial starting location, and returns the [Uri] that the [DocumentsProvider]
+     * returns in the [Intent] via its [Intent.getData] (aka kotlin `data` property) to the lambda
+     * argument of [registerForActivityResult].
+     *
+     * In the lambda argument to [registerForActivityResult] we initialize our [Uri] variable
+     * `val documentUri` to the `uri` argument to the lambda, returning to [registerForActivityResult]
+     * if this is `null` (the user returned to this fragment without selecting a folder). We initialize
+     * our [Context] variable `val context` to the [Context] of the single, global Application object
+     * of the current process. We initialize our [DocumentFile] variable `val parentFolder` to the
+     * instance that the [DocumentFile.fromTreeUri] method creates to represent the document tree
+     * rooted at the [Uri] `documentUri` again returning to [registerForActivityResult] if this is
+     * `null`. We get a [LifecycleOwner] that represents the [Fragment]'s View lifecycle, and use
+     * the [CoroutineScope] tied to this [LifecycleOwner]'s Lifecycle to launch a new coroutine block.
+     * We initialize our [String] variable `val text` to a string that we build by calling the
+     * [SafFragmentViewModel.listFiles] method of [viewModel] to retrieve the list of [Pair] that it
+     * forms from the [String] name of the files in `parentFolder` and the [Uri] to that file, then
+     * sorting that list on the [Pair.first] entries, and then joining all of the [Pair.first] names
+     * in a [String] separated by the default ", " separator. Finally we set the text of the [TextView]
+     * found at the [FragmentSafBinding.output] field of [binding] to a [String] formed by
+     * concatenating the string "Folder contents:" to `text`.
+     */
     private val actionOpenDocumentTree = registerForActivityResult(OpenDocumentTree()) { uri ->
-        val documentUri = uri ?: return@registerForActivityResult
-        val context = requireContext().applicationContext
-        val parentFolder = DocumentFile.fromTreeUri(context, documentUri)
+        val documentUri: Uri = uri ?: return@registerForActivityResult
+        val context: Context = requireContext().applicationContext
+        val parentFolder: DocumentFile = DocumentFile.fromTreeUri(context, documentUri)
             ?: return@registerForActivityResult
 
         viewLifecycleOwner.lifecycleScope.launch {
-            val text = viewModel.listFiles(parentFolder)
+            val text: String = viewModel.listFiles(parentFolder)
                 .sortedBy { it.first }
                 .joinToString { it.first }
             binding.output.text = getString(R.string.saf_folder_output, text)
