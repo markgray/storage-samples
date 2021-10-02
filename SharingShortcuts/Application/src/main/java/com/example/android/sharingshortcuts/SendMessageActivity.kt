04f7e563d5d9e48bc9252eecc43537255e161d14
markgray <mark.t.gray@gmail.com>
2021-10-02 07:49:40 -0400

    Converted to kotlin: SendMessageActivity.kt in SharingShortcuts

--- SharingShortcuts/Application/src/main/java/com/example/android/sharingshortcuts/SendMessageActivity.kt
+++ SharingShortcuts/Application/src/main/java/com/example/android/sharingshortcuts/SendMessageActivity.kt
@@ -13,152 +13,140 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+package com.example.android.sharingshortcuts
 
-package com.example.android.sharingshortcuts;
-
-import android.app.Activity;
-import android.content.Intent;
-import android.os.Build;
-import android.os.Bundle;
-import android.view.View;
-import android.widget.TextView;
-import android.widget.Toast;
-
-import androidx.core.content.pm.ShortcutManagerCompat;
+import android.app.Activity
+import android.content.Intent
+import android.os.Build
+import android.os.Bundle
+import android.view.View
+import android.widget.TextView
+import android.widget.Toast
+import androidx.core.content.pm.ShortcutManagerCompat
 
 /**
- * Provides the UI for sharing a text with a {@link Contact}.
+ * Provides the UI for sharing a text with a [Contact].
  */
-public class SendMessageActivity extends Activity {
-
-    /**
-     * The request code for {@link SelectContactActivity}. This is used when the user doesn't
-     * select any of Direct Share icons.
-     */
-    private static final int REQUEST_SELECT_CONTACT = 1;
-
+class SendMessageActivity : Activity() {
     /**
      * The text to share.
      */
-    private String mBody;
+    private var mBody: String? = null
 
     /**
      * The ID of the contact to share the text with.
      */
-    private int mContactId;
+    private var mContactId = 0
 
     // View references.
-    private TextView mTextContactName;
-    private TextView mTextMessageBody;
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_send_message);
-        setTitle(R.string.sending_message);
+    private lateinit var mTextContactName: TextView
+    private lateinit var mTextMessageBody: TextView
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_send_message)
+        setTitle(R.string.sending_message)
         // View references.
-        mTextContactName = findViewById(R.id.contact_name);
-        mTextMessageBody = findViewById(R.id.message_body);
+        mTextContactName = findViewById(R.id.contact_name)
+        mTextMessageBody = findViewById(R.id.message_body)
         // Handle the share Intent.
-        boolean handled = handleIntent(getIntent());
+        val handled = handleIntent(intent)
         if (!handled) {
-            finish();
-            return;
+            finish()
+            return
         }
         // Bind event handlers.
-        findViewById(R.id.send).setOnClickListener(mOnClickListener);
+        findViewById<View>(R.id.send).setOnClickListener(mOnClickListener)
         // Set up the UI.
-        prepareUi();
+        prepareUi()
         // The contact ID will not be passed on when the user clicks on the app icon rather than any
         // of the Direct Share icons. In this case, we show another dialog for selecting a contact.
         if (mContactId == Contact.INVALID_ID) {
-            selectContact();
+            selectContact()
         }
     }
 
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        switch (requestCode) {
-            case REQUEST_SELECT_CONTACT:
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent) {
+        when (requestCode) {
+            REQUEST_SELECT_CONTACT -> {
                 if (resultCode == RESULT_OK) {
-                    mContactId = data.getIntExtra(Contact.ID, Contact.INVALID_ID);
+                    mContactId = data.getIntExtra(Contact.ID, Contact.INVALID_ID)
                 }
                 // Give up sharing the send_message if the user didn't choose a contact.
                 if (mContactId == Contact.INVALID_ID) {
-                    finish();
-                    return;
+                    finish()
+                    return
                 }
-                prepareUi();
-                break;
-            default:
-                super.onActivityResult(requestCode, resultCode, data);
+                prepareUi()
+            }
+            else -> super.onActivityResult(requestCode, resultCode, data)
         }
     }
 
     /**
-     * Handles the passed {@link Intent}. This method can only handle intents for sharing a plain
-     * text. {@link #mBody} and {@link #mContactId} are modified accordingly.
+     * Handles the passed [Intent]. This method can only handle intents for sharing a plain
+     * text. [.mBody] and [.mContactId] are modified accordingly.
      *
-     * @param intent The {@link Intent}.
-     * @return true if the {@code intent} is handled properly.
+     * @param intent The [Intent].
+     * @return true if the `intent` is handled properly.
      */
-    private boolean handleIntent(Intent intent) {
-        if (Intent.ACTION_SEND.equals(intent.getAction())
-                && "text/plain".equals(intent.getType())) {
-            mBody = intent.getStringExtra(Intent.EXTRA_TEXT);
+    private fun handleIntent(intent: Intent): Boolean {
+        if (Intent.ACTION_SEND == intent.action && "text/plain" == intent.type) {
+            mBody = intent.getStringExtra(Intent.EXTRA_TEXT)
             // The intent comes from Direct share
-            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.P
-                    && intent.hasExtra(ShortcutManagerCompat.EXTRA_SHORTCUT_ID)) {
-                String shortcutId = intent.getStringExtra(ShortcutManagerCompat.EXTRA_SHORTCUT_ID);
-                mContactId = Integer.valueOf(shortcutId);
+            mContactId = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.P
+                && intent.hasExtra(ShortcutManagerCompat.EXTRA_SHORTCUT_ID)) {
+                val shortcutId = intent.getStringExtra(ShortcutManagerCompat.EXTRA_SHORTCUT_ID)
+                Integer.valueOf(shortcutId!!)
             } else {
                 // The text was shared and the user chose our app
-                mContactId = Contact.INVALID_ID;
+                Contact.INVALID_ID
             }
-            return true;
+            return true
         }
-        return false;
+        return false
     }
 
     /**
      * Sets up the UI.
      */
-    private void prepareUi() {
+    private fun prepareUi() {
         if (mContactId != Contact.INVALID_ID) {
-            Contact contact = Contact.byId(mContactId);
-            ContactViewBinder.bind(contact, mTextContactName);
+            val contact = Contact.byId(mContactId)
+            ContactViewBinder.bind(contact, mTextContactName)
         }
-        mTextMessageBody.setText(mBody);
+        mTextMessageBody.text = mBody
     }
 
     /**
-     * Delegates selection of a {@Contact} to {@link SelectContactActivity}.
+     * Delegates selection of a {@Contact} to [SelectContactActivity].
      */
-    private void selectContact() {
-        Intent intent = new Intent(this, SelectContactActivity.class);
-        intent.setAction(SelectContactActivity.ACTION_SELECT_CONTACT);
-        startActivityForResult(intent, REQUEST_SELECT_CONTACT);
+    private fun selectContact() {
+        val intent = Intent(this, SelectContactActivity::class.java)
+        intent.action = SelectContactActivity.ACTION_SELECT_CONTACT
+        startActivityForResult(intent, REQUEST_SELECT_CONTACT)
     }
 
-    private View.OnClickListener mOnClickListener = new View.OnClickListener() {
-        @Override
-        public void onClick(View view) {
-            switch (view.getId()) {
-                case R.id.send:
-                    send();
-                    break;
-            }
+    private val mOnClickListener = View.OnClickListener { view ->
+        when (view.id) {
+            R.id.send -> send()
         }
-    };
+    }
 
     /**
      * Pretends to send the text to the contact. This only shows a dummy message.
      */
-    private void send() {
+    private fun send() {
         Toast.makeText(this,
-                getString(R.string.message_sent, mBody, Contact.byId(mContactId).getName()),
-                Toast.LENGTH_SHORT).show();
-        finish();
+            getString(R.string.message_sent, mBody, Contact.byId(mContactId).name),
+            Toast.LENGTH_SHORT).show()
+        finish()
     }
 
+    companion object {
+        /**
+         * The request code for [SelectContactActivity]. This is used when the user doesn't
+         * select any of Direct Share icons.
+         */
+        private const val REQUEST_SELECT_CONTACT = 1
+    }
 }
