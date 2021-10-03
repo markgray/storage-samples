0a9cf48899f972be36fc395cad82cb81c58451de
markgray <mark.t.gray@gmail.com>
2021-10-03 06:24:28 -0400

    Converted to kotlin SelectContactActivity.kt in SharingShortcuts

--- SharingShortcuts/Application/src/main/java/com/example/android/sharingshortcuts/SelectContactActivity.kt
+++ SharingShortcuts/Application/src/main/java/com/example/android/sharingshortcuts/SelectContactActivity.kt
@@ -13,83 +13,71 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+package com.example.android.sharingshortcuts
 
-package com.example.android.sharingshortcuts;
-
-import android.app.Activity;
-import android.content.Intent;
-import android.os.Bundle;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.TextView;
-
-import androidx.annotation.NonNull;
-import androidx.recyclerview.widget.LinearLayoutManager;
-import androidx.recyclerview.widget.RecyclerView;
+import android.app.Activity
+import android.content.Intent
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import androidx.recyclerview.widget.RecyclerView.ViewHolder
 
 /**
  * The dialog for selecting a contact to share the text with. This dialog is shown when the user
  * taps on this sample's icon rather than any of the Direct Share contacts.
  */
-public class SelectContactActivity extends Activity {
-
-    /**
-     * The action string for Intents.
-     */
-    public static final String ACTION_SELECT_CONTACT =
-            "com.example.android.sharingshortcuts.intent.action.SELECT_CONTACT";
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_select_contact);
-        Intent intent = getIntent();
-        if (!ACTION_SELECT_CONTACT.equals(intent.getAction())) {
-            finish();
-            return;
+class SelectContactActivity : Activity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_select_contact)
+        val intent = intent
+        if (ACTION_SELECT_CONTACT != intent.action) {
+            finish()
+            return
         }
         // Set up the list of contacts
-        RecyclerView recyclerView = findViewById(R.id.recycler_view);
-        recyclerView.setAdapter(mContactAdapter);
-        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        val recyclerView: RecyclerView = findViewById(R.id.recycler_view)
+        recyclerView.adapter = mContactAdapter
+        recyclerView.layoutManager = LinearLayoutManager(this)
     }
 
-    private final RecyclerView.Adapter<ContactViewHolder> mContactAdapter =
-            new RecyclerView.Adapter<ContactViewHolder>() {
-
-                @NonNull
-                @Override
-                public ContactViewHolder onCreateViewHolder(@NonNull ViewGroup parent,
-                                                            int viewType) {
-                    TextView textView = (TextView) LayoutInflater.from(parent.getContext())
-                            .inflate(R.layout.item_contact, parent, false);
-                    return new ContactViewHolder(textView);
-                }
-
-                @Override
-                public void onBindViewHolder(@NonNull ContactViewHolder holder,
-                                             final int position) {
-                    Contact contact = Contact.CONTACTS[position];
-                    ContactViewBinder.bind(contact, (TextView) holder.itemView);
-                    holder.itemView.setOnClickListener((View view) -> {
-                        Intent data = new Intent();
-                        data.putExtra(Contact.ID, holder.getBindingAdapterPosition());
-                        setResult(RESULT_OK, data);
-                        finish();
-                    });
-                }
-
-                @Override
-                public int getItemCount() {
-                    return Contact.CONTACTS.length;
-                }
-            };
-
-    private static class ContactViewHolder extends RecyclerView.ViewHolder {
-
-        ContactViewHolder(@NonNull TextView textView) {
-            super(textView);
+    private val mContactAdapter: RecyclerView.Adapter<ContactViewHolder> = object : RecyclerView.Adapter<ContactViewHolder>() {
+        override fun onCreateViewHolder(
+            parent: ViewGroup,
+            viewType: Int
+        ): ContactViewHolder {
+            val textView = LayoutInflater.from(parent.context)
+                .inflate(R.layout.item_contact, parent, false) as TextView
+            return ContactViewHolder(textView)
         }
+
+        override fun onBindViewHolder(
+            holder: ContactViewHolder,
+            position: Int
+        ) {
+            val contact = Contact.CONTACTS[position]
+            ContactViewBinder.bind(contact, holder.itemView as TextView)
+            holder.itemView.setOnClickListener {
+                val data = Intent()
+                data.putExtra(Contact.ID, holder.bindingAdapterPosition)
+                setResult(RESULT_OK, data)
+                finish()
+            }
+        }
+
+        override fun getItemCount(): Int {
+            return Contact.CONTACTS.size
+        }
+    }
+
+    private class ContactViewHolder(textView: TextView) : ViewHolder(textView)
+    companion object {
+        /**
+         * The action string for Intents.
+         */
+        const val ACTION_SELECT_CONTACT = "com.example.android.sharingshortcuts.intent.action.SELECT_CONTACT"
     }
 }
