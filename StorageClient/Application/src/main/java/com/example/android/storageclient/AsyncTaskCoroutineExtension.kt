8faff6ef4715271a0800b18499d0e51c0e27c742
markgray <mark.t.gray@gmail.com>
2021-10-05 07:06:58 -0400

    Converted AsyncTask to coroutine in StorageClientFragment.kt in StorageClient

---
+++ StorageClient/Application/src/main/java/com/example/android/storageclient/AsyncTaskCoroutineExtension.kt
@@ -0,0 +1,19 @@
+package com.example.android.storageclient
+
+import android.graphics.Bitmap
+import android.net.Uri
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+
+fun CoroutineScope.GetBitmapAndDisplay(
+    doInBackground: (uri: Uri) -> Bitmap,
+    onPostExecute: (Bitmap) -> Unit,
+    uri: Uri
+) = launch {
+    val result = withContext(Dispatchers.IO) { // runs in background thread without blocking the Main Thread
+        doInBackground(uri)
+    }
+    onPostExecute(result)
+}
--- StorageClient/Application/src/main/java/com/example/android/storageclient/StorageClientFragment.kt
+++ StorageClient/Application/src/main/java/com/example/android/storageclient/StorageClientFragment.kt
@@ -18,10 +18,10 @@ package com.example.android.storageclient
 import android.app.Activity
 import android.app.Dialog
 import android.content.Intent
+import android.database.Cursor
 import android.graphics.Bitmap
 import android.graphics.BitmapFactory
 import android.net.Uri
-import android.os.AsyncTask
 import android.os.Bundle
 import android.os.ParcelFileDescriptor
 import android.provider.OpenableColumns
@@ -33,6 +33,7 @@ import androidx.activity.result.ActivityResultLauncher
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.fragment.app.DialogFragment
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.lifecycleScope
 import com.example.android.common.logger.Log.e
 import com.example.android.common.logger.Log.i
 import java.io.IOException
@@ -60,10 +61,11 @@ class StorageClientFragment : Fragment() {
                 }
             }
         }
+
     /**
      * Fires an intent to spin up the "file chooser" UI and select an image.
      */
-    fun performFileSearch() {
+    private fun performFileSearch() {
         // ACTION_OPEN_DOCUMENT is the intent to choose a file via the system's file browser.
         val intent = Intent(Intent.ACTION_OPEN_DOCUMENT)
 
@@ -84,7 +86,7 @@ class StorageClientFragment : Fragment() {
      *
      * @param uri the Uri of the image to display.
      */
-    fun showImage(uri: Uri?) {
+    private fun showImage(uri: Uri?) {
         // BEGIN_INCLUDE (create_show_image_dialog)
         if (uri != null) {
             // Since the URI is to an image, create and show a DialogFragment to display the
@@ -144,25 +146,20 @@ class StorageClientFragment : Fragment() {
             val imageView = ImageView(activity)
             mDialog!!.setContentView(imageView)
 
-            // BEGIN_INCLUDE (show_image)
             // Loading the image is going to require some sort of I/O, which must occur off the UI
             // thread.  Changing the ImageView to display the image must occur ON the UI thread.
             // The easiest way to divide up this labor is with an AsyncTask.  The doInBackground
             // method will run in a separate thread, but onPostExecute will run in the main
             // UI thread.
-            val imageLoadAsyncTask: AsyncTask<Uri?, Void?, Bitmap?> =
-                object : AsyncTask<Uri?, Void?, Bitmap?>() {
-                    protected override fun doInBackground(vararg uris: Uri?): Bitmap? {
-                        dumpImageMetaData(uris[0])
-                        return getBitmapFromUri(uris[0]!!)
-                    }
-
-                    override fun onPostExecute(bitmap: Bitmap?) {
-                        imageView.setImageBitmap(bitmap)
-                    }
-                }
-            imageLoadAsyncTask.execute(mUri)
-            // END_INCLUDE (show_image)
+            lifecycleScope.GetBitmapAndDisplay(
+                doInBackground = { uri: Uri ->
+                    dumpImageMetaData(uri)
+                    getBitmapFromUri(uri)!!
+                }, onPostExecute = { bitmap: Bitmap ->
+                imageView.setImageBitmap(bitmap)
+            },
+                mUri!!
+            )
             return mDialog!!
         }
 
@@ -186,43 +183,37 @@ class StorageClientFragment : Fragment() {
             // document.
             val cursor = requireActivity().contentResolver
                 .query(uri!!, null, null, null, null, null)
-            try {
-                // moveToFirst() returns false if the cursor has 0 rows.  Very handy for
+            cursor?.use { cursorIt: Cursor ->
+                // moveToFirst() returns false if the cursorIt has 0 rows.  Very handy for
                 // "if there's anything to look at, look at it" conditionals.
-                if (cursor != null && cursor.moveToFirst()) {
+                if (cursorIt.moveToFirst()) {
 
                     // Note it's called "Display Name".  This is provider-specific, and
                     // might not necessarily be the file name.
-                    val displayName = cursor.getString(
-                        cursor.getColumnIndex(OpenableColumns.DISPLAY_NAME))
+                    val displayName = cursorIt.getString(
+                        cursorIt.getColumnIndex(OpenableColumns.DISPLAY_NAME))
                     i(TAG, "Display Name: $displayName")
-                    val sizeIndex = cursor.getColumnIndex(OpenableColumns.SIZE)
+                    val sizeIndex = cursorIt.getColumnIndex(OpenableColumns.SIZE)
                     // If the size is unknown, the value stored is null.  But since an int can't be
                     // null in java, the behavior is implementation-specific, which is just a fancy
                     // term for "unpredictable".  So as a rule, check if it's null before assigning
                     // to an int.  This will happen often:  The storage API allows for remote
                     // files, whose size might not be locally known.
-                    val size: String?
-                    size = if (!cursor.isNull(sizeIndex)) {
-                        // Technically the column stores an int, but cursor.getString will do the
+                    val size: String? = if (!cursorIt.isNull(sizeIndex)) {
+                        // Technically the column stores an int, but cursorIt.getString will do the
                         // conversion automatically.
-                        cursor.getString(sizeIndex)
+                        cursorIt.getString(sizeIndex)
                     } else {
                         "Unknown"
                     }
                     i(TAG, "Size: $size")
                 }
-            } finally {
-                cursor?.close()
             }
             // END_INCLUDE (dump_metadata)
         }
     }
 
     companion object {
-        // A request code's purpose is to match the result of a "startActivityForResult" with
-        // the type of the original request.  Choose any value.
-        private const val READ_REQUEST_CODE = 1337
         const val TAG = "StorageClientFragment"
     }
 }
