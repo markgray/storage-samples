a628b782311d94a0df4487a78d48ac1e531b5d8f
markgray <mark.t.gray@gmail.com>
2021-10-05 08:02:44 -0400

    Added comments to AsyncTaskCoroutineExtension.kt in StorageClient

--- StorageClient/Application/src/main/java/com/example/android/storageclient/AsyncTaskCoroutineExtension.kt
+++ StorageClient/Application/src/main/java/com/example/android/storageclient/AsyncTaskCoroutineExtension.kt
@@ -6,7 +6,18 @@ import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
+import kotlin.coroutines.CoroutineContext
 
+/**
+ * Simple [CoroutineScope] extension function which fetches a [Bitmap] using the [CoroutineContext]
+ * of [Dispatchers.IO] to execute the [doInBackground] lambda using its [Uri] parameter [uri] and
+ * then calls the [onPostExecute] lambda with the [Bitmap] returned from [doInBackground] when the
+ * background thread completes.
+ *
+ * @param doInBackground a lambda which takes a [Uri] and returns a [Bitmap].
+ * @param onPostExecute a lambda which takes a [Bitmap] and returns nothing.
+ * @param uri the [Uri] that will be passed to the [doInBackground] lambda.
+ */
 fun CoroutineScope.getBitmapAndDisplay(
     doInBackground: (uri: Uri) -> Bitmap,
     onPostExecute: (Bitmap) -> Unit,
