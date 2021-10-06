2c026dad3e670aac687e992b9d6f61ffc6ce697f
markgray <mark.t.gray@gmail.com>
2021-10-06 06:40:52 -0400

    Added comments to StorageClientFragment.kt in StorageClient

--- StorageClient/Application/src/main/java/com/example/android/storageclient/StorageClientFragment.kt
+++ StorageClient/Application/src/main/java/com/example/android/storageclient/StorageClientFragment.kt
@@ -35,9 +35,13 @@ import androidx.activity.result.contract.ActivityResultContracts
 import androidx.fragment.app.DialogFragment
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentManager
+import androidx.lifecycle.LifecycleOwner
 import androidx.lifecycle.lifecycleScope
 import com.example.android.common.logger.Log
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
 import java.io.IOException
+import kotlin.coroutines.CoroutineContext
 
 /**
  * [Fragment] which has the user choose an image file using the system's file browser and displays
@@ -156,24 +160,54 @@ class StorageClientFragment : Fragment() {
      * [DialogFragment] which displays an image, given a content URI.
      */
     class ImageDialogFragment : DialogFragment() {
+        /**
+         * The [Dialog] that holds the [ImageView] that we use to display our image.
+         */
         private var mDialog: Dialog? = null
+
+        /**
+         * The content [Uri] for the image we are to fetch and display in our [Dialog] field [mDialog]
+         */
         private var mUri: Uri? = null
+
+        /**
+         * Called to do initial creation of a [DialogFragment]. This is called after [onAttach] and
+         * before [onCreateView]. First we call our super's implementation of `onCreate`, then we
+         * initialize our [Uri] field [mUri] by retrieving the arguments [Bundle] supplied when our
+         * fragment was instantiated using the [requireArguments] method (which throws the exception
+         * [IllegalStateException] if no arguments were supplied) and using the [Bundle.getParcelable]
+         * method on that [Bundle] to retrieve the [Uri] stored under the key "URI" in it.
+         *
+         * @param savedInstanceState we do not override [onSaveInstanceState] so do not use.
+         */
         override fun onCreate(savedInstanceState: Bundle?) {
             super.onCreate(savedInstanceState)
             mUri = requireArguments().getParcelable("URI")
         }
 
         /**
-         * Create a Bitmap from the URI for that image and return it.
+         * Create a [Bitmap] from the content [Uri] parameter [uri] for that image and return it. We
+         * initialize our [ParcelFileDescriptor] variable `var parcelFileDescriptor` to `null` then
+         * we return the [Bitmap] returned by a `try` block intended to catch and log any exception
+         * (the `catch` block returns `null` as the result). In the `try` block we set our variable
+         * `parcelFileDescriptor` to the [ParcelFileDescriptor] returned by a [ContentResolver]
+         * instance for our application's package when we call its [ContentResolver.openFileDescriptor]
+         * method to open a raw file descriptor to read data from a our [Uri] parameter [uri]. We
+         * initialize our [Bitmap] variable `val image` to the [Bitmap] decoded by the method
+         * [BitmapFactory.decodeFileDescriptor] from the file descriptor `fileDescriptor`. We then
+         * close `parcelFileDescriptor` and return `image` as the result of the `try` block. In the
+         * `finally` block of the `try` we `try` to close `parcelFileDescriptor` and if that throws
+         * an [IOException] we print the [IOException] and its backtrace to the standard error stream
+         * and log the error.
          *
-         * @param uri the Uri for the image to return.
+         * @param uri the [Uri] for the image to return.
          */
         private fun getBitmapFromUri(uri: Uri): Bitmap? {
             var parcelFileDescriptor: ParcelFileDescriptor? = null
             return try {
                 parcelFileDescriptor = requireActivity().contentResolver.openFileDescriptor(uri, "r")
                 val fileDescriptor = parcelFileDescriptor!!.fileDescriptor
-                val image = BitmapFactory.decodeFileDescriptor(fileDescriptor)
+                val image: Bitmap = BitmapFactory.decodeFileDescriptor(fileDescriptor)
                 parcelFileDescriptor.close()
                 image
             } catch (e: Exception) {
@@ -189,6 +223,32 @@ class StorageClientFragment : Fragment() {
             }
         }
 
+        /**
+         * Override to build your own custom [Dialog] container. This method will be called after
+         * [onCreate] and immediately before [onCreateView].
+         *
+         * First we set our [Dialog] field [mDialog] to the instance create by our super's
+         * implementation of [onCreateDialog]. Then we retrieve the current [Window] of [mDialog]
+         * and request the feature [Window.FEATURE_NO_TITLE] (the "no title" feature, turning off
+         * the title at the top of the screen). WE initialize our [ImageView] variable `val imageView`
+         * to a new instance and set it to be the content view of [mDialog].
+         *
+         * Next we use the [CoroutineScope.getBitmapAndDisplay] extension function on the
+         * [CoroutineScope] tied to this [LifecycleOwner]'s Lifecycle supplying a lambda for
+         * its `doInBackground` argument that takes a [Uri], calls our [dumpImageMetaData] method
+         * on it and returns the [Bitmap] that our [getBitmapFromUri] method creates when it
+         * fetches and decodes the image from the [Uri]. The `doInBackground` lambda is exectuted
+         * on the [CoroutineContext] of [Dispatchers.IO] and the [Bitmap] it returns is then used
+         * as the argument to the `onPostExecute` lambda which is run on the UI thread and the
+         * `onPostExecute` lambda we use will call the [ImageView.setImageBitmap] method of
+         * `imageView` with that [Bitmap] to set it as the content of the [ImageView]. For the
+         * [Uri] argument of [CoroutineScope.getBitmapAndDisplay] we pass it our [Uri] field [mUri].
+         *
+         * Finally we return our [Dialog] field [mDialog] to the caller.
+         *
+         * @param savedInstanceState we do not override [onSaveInstanceState] so do not use.
+         * @return Return a new [Dialog] instance to be displayed by the [Fragment].
+         */
         override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
             mDialog = super.onCreateDialog(savedInstanceState)
             // To optimize for the "lightbox" style layout.  Since we're not actually displaying a
@@ -199,22 +259,31 @@ class StorageClientFragment : Fragment() {
             mDialog!!.setContentView(imageView)
 
             // Loading the image is going to require some sort of I/O, which must occur off the UI
-            // thread.  Changing the ImageView to display the image must occur ON the UI thread.
-            // The easiest way to divide up this labor is with an AsyncTask.  The doInBackground
-            // method will run in a separate thread, but onPostExecute will run in the main
-            // UI thread.
+            // thread. Changing the ImageView to display the image must occur ON the UI thread.
+            // The easiest way to divide up this labor is with an AsyncTask but that is deprecated.
+            // Instead we use the CoroutineScope extension function getBitmapAndDisplay which takes
+            // two lambdas and the Uri to fetch, decode and display. The doInBackground lambda will
+            // run on the Dispatchers.IO CoroutineContext in a separate thread, but the onPostExecute
+            // lambda will be run in the main UI thread.
             lifecycleScope.getBitmapAndDisplay(
                 doInBackground = { uri: Uri ->
                     dumpImageMetaData(uri)
                     getBitmapFromUri(uri)!!
-                }, onPostExecute = { bitmap: Bitmap ->
-                imageView.setImageBitmap(bitmap)
-            },
+                },
+                onPostExecute = { bitmap: Bitmap ->
+                    imageView.setImageBitmap(bitmap)
+                },
                 mUri!!
             )
             return mDialog!!
         }
 
+        /**
+         * Called when the [DialogFragment] is no longer started. First we call our super's
+         * implementation of `onStop`, then if the [DialogFragment.getDialog] method (aka kotlin
+         * `dialog` property) is not `null` we call its [Dialog.dismiss] method to dismiss the
+         * dialog, removing it from the screen.
+         */
         override fun onStop() {
             super.onStop()
             if (dialog != null) {
@@ -223,15 +292,28 @@ class StorageClientFragment : Fragment() {
         }
 
         /**
-         * Grabs metadata for a document specified by URI, logs it to the screen.
+         * Grabs metadata for a document specified by [Uri], logs it to the screen. We initialize
+         * our [Cursor] variable `val cursor` to the [Cursor] returned by a [ContentResolver]
+         * instance for our application's package when we call its [ContentResolver.query] method
+         * on our [Uri] parameter [uri]. If `cursor` is not `null` we use the [use] extension method
+         * on it to execute a block wherein:
+         *  - we call the [Cursor.moveToFirst] method on it to move it to the first row, and it that
+         *  method returns `false` the [Cursor] is empty so we do nothing.
+         *  - otherwise we initialize our [String] variable `val displayName` to the value stored in
+         *  the [OpenableColumns.DISPLAY_NAME] column and we log `displayName`.
+         *  - we initialize our [Int] variable `val sizeIndex` to the column index for the
+         *  [OpenableColumns.SIZE] column.
+         *  - we initialize our [String] variable `val size` to the [String] value of the column
+         *  at index `sizeIndex` if it is not `null`, or to the [String] "Unknown" if it is `null`.
+         *  - Finally we log `size`.
          *
-         * @param uri The uri for the document whose metadata should be printed.
+         * @param uri The [Uri] for the document whose metadata should be printed.
          */
         fun dumpImageMetaData(uri: Uri?) {
             // The query, since it only applies to a single document, will only return one row.
             // no need to filter, sort, or select fields, since we want all fields for one
             // document.
-            val cursor = requireActivity().contentResolver
+            val cursor: Cursor? = requireActivity().contentResolver
                 .query(uri!!, null, null, null, null, null)
             cursor?.use { cursorIt: Cursor ->
                 // moveToFirst() returns false if the cursorIt has 0 rows.  Very handy for
@@ -240,10 +322,11 @@ class StorageClientFragment : Fragment() {
 
                     // Note it's called "Display Name".  This is provider-specific, and
                     // might not necessarily be the file name.
-                    val displayName = cursorIt.getString(
-                        cursorIt.getColumnIndex(OpenableColumns.DISPLAY_NAME))
+                    val displayName: String = cursorIt.getString(
+                        cursorIt.getColumnIndex(OpenableColumns.DISPLAY_NAME)
+                    )
                     Log.i(TAG, "Display Name: $displayName")
-                    val sizeIndex = cursorIt.getColumnIndex(OpenableColumns.SIZE)
+                    val sizeIndex: Int = cursorIt.getColumnIndex(OpenableColumns.SIZE)
                     // If the size is unknown, the value stored is null.  But since an int can't be
                     // null in java, the behavior is implementation-specific, which is just a fancy
                     // term for "unpredictable".  So as a rule, check if it's null before assigning
