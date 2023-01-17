b00ceebdbdefb146deac948d7545aa53d19c8185
Mark Gray <mark.t.gray@gmail.com>
2023-01-17 06:34:14 -0500

    Updated to electric eel in ContentProviderPaging

--- ContentProviderPaging/app/src/main/java/com/example/android/contentproviderpaging/ImageClientFragment.kt
+++ ContentProviderPaging/app/src/main/java/com/example/android/contentproviderpaging/ImageClientFragment.kt
@@ -15,6 +15,7 @@
  */
 package com.example.android.contentproviderpaging
 
+import android.annotation.SuppressLint
 import android.app.Activity
 import android.content.ContentProvider
 import android.content.ContentResolver
@@ -187,6 +188,7 @@ class ImageClientFragment : Fragment() {
          * @param args Any arguments supplied by the caller.
          * @return Return a new [Loader] instance that is ready to start loading.
          */
+        @SuppressLint("StaticFieldLeak") // TODO: Fix Loader class should be static or leaks might occur
         override fun onCreateLoader(id: Int, args: Bundle?): Loader<Cursor> {
             val activity: Activity? = this@ImageClientFragment.activity
             return object : CursorLoader(activity as Context) {
