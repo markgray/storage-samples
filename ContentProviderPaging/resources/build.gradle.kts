a2ebb50c290ef425a7f23a92a865b55cbeee14c3
markgray <mark.t.gray@gmail.com>
2024-07-01 07:02:43 -0400

    Converted build files to kts, and updated AGP to 8.5.0 in ContentProviderPaging

---
+++ ContentProviderPaging/resources/build.gradle.kts
@@ -0,0 +1,42 @@
+plugins {
+    id("com.android.library")
+    id("org.jetbrains.kotlin.android")
+}
+
+android {
+    compileSdk = 34
+    defaultConfig {
+        minSdk = 21
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+    kotlinOptions {
+        jvmTarget = "17"
+    }
+    namespace = "com.example.android.contentproviderpaging.common"
+}
+
+dependencies {
+    api("androidx.appcompat:appcompat:1.7.0")
+    api("androidx.recyclerview:recyclerview:1.3.2")
+    api("com.github.bumptech.glide:glide:4.16.0")
+    testImplementation("junit:junit:4.13.2")
+    implementation("androidx.core:core-ktx:1.13.1")
+}
+
+repositories {
+    google()
+    mavenCentral()
+}
