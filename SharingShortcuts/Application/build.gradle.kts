df297a73c9715f16e7a8150b9216ff8498d3a90f
markgray <mark.t.gray@gmail.com>
2024-07-02 06:45:39 -0400

    Converted build files to kts, and updated AGP to 8.5.0 in SharingShortcuts

---
+++ SharingShortcuts/Application/build.gradle.kts
@@ -0,0 +1,27 @@
+plugins {
+    id("com.android.application")
+    id("org.jetbrains.kotlin.android")
+}
+
+dependencies {
+    implementation("androidx.appcompat:appcompat:1.7.0")
+    implementation("androidx.sharetarget:sharetarget:1.2.0")
+    implementation("androidx.recyclerview:recyclerview:1.3.2")
+    implementation("androidx.core:core-ktx:1.13.1")
+}
+
+android {
+    compileSdk = 34
+    defaultConfig {
+        minSdk = 21
+        targetSdk = 34
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+    kotlinOptions {
+        jvmTarget = "17"
+    }
+    namespace = "com.example.android.sharingshortcuts"
+}
