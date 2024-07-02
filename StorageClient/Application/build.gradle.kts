7a0ed9cf1fb1d8451f06543d8dd684ad218a51ab
markgray <mark.t.gray@gmail.com>
2024-07-02 06:58:46 -0400

    Converted build files to kts, updated AGP to 8.5.0, and updated gradle dependencies in StorageClient

---
+++ StorageClient/Application/build.gradle.kts
@@ -0,0 +1,29 @@
+plugins {
+    id("com.android.application")
+    id("org.jetbrains.kotlin.android")
+}
+
+dependencies {
+    implementation("androidx.cardview:cardview:1.0.0")
+    implementation("androidx.appcompat:appcompat:1.7.0")
+    implementation("androidx.core:core-ktx:1.13.1")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.8.1")
+    implementation("androidx.fragment:fragment-ktx:1.8.1")
+    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.8.3")
+}
+
+android {
+    compileSdk = 34
+    defaultConfig {
+        minSdk = 21
+        targetSdk = 34
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+    kotlinOptions {
+        jvmTarget = "17"
+    }
+    namespace = "com.example.android.storageclient"
+}
