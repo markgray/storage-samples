019771457efbf3c5f402bd71b797dc160650d92e
Mark Gray <mark.t.gray@gmail.com>
2024-07-25 04:07:37 -0400

    Updated gradle dependency in MediaStore

--- MediaStore/app/build.gradle.kts
+++ MediaStore/app/build.gradle.kts
@@ -57,7 +57,7 @@ dependencies {
     implementation("com.google.android.material:material:1.12.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
     implementation("androidx.vectordrawable:vectordrawable:1.2.0")
-    implementation("androidx.activity:activity-ktx:1.9.0")
+    implementation("androidx.activity:activity-ktx:1.9.1")
     implementation("com.github.bumptech.glide:glide:4.16.0")
 
     testImplementation("junit:junit:4.13.2")
