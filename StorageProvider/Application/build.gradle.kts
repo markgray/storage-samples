27015b56f48659a6cf93e24f5ae87849e90be45a
Mark Gray <mark.t.gray@gmail.com>
2024-10-24 03:20:16 -0400

    Updated AGP to 8.7.1 and updated gradle dependency in StorageProvider

--- StorageProvider/Application/build.gradle.kts
+++ StorageProvider/Application/build.gradle.kts
@@ -20,7 +20,7 @@ plugins {
 }
 
 dependencies {
-    implementation("androidx.activity:activity-ktx:1.9.2")
+    implementation("androidx.activity:activity-ktx:1.9.3")
     implementation("androidx.cardview:cardview:1.0.0")
     implementation("androidx.appcompat:appcompat:1.7.0")
     implementation("androidx.core:core-ktx:1.13.1")
