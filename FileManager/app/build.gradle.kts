12c802f2318661ffd6b3c74c1da496884db89f09
Mark Gray <mark.t.gray@gmail.com>
2025-03-04 04:02:51 -0500

    Updated AGP to 8.8.2 and updated gradle dependencies in FileManager

--- FileManager/app/build.gradle.kts
+++ FileManager/app/build.gradle.kts
@@ -53,10 +53,10 @@ android {
 }
 
 dependencies {
-    implementation("androidx.activity:activity-ktx:1.10.0")
+    implementation("androidx.activity:activity-ktx:1.10.1")
     implementation("androidx.appcompat:appcompat:1.7.0")
     implementation("androidx.core:core-ktx:1.15.0")
-    implementation("androidx.constraintlayout:constraintlayout:2.2.0")
+    implementation("androidx.constraintlayout:constraintlayout:2.2.1")
     implementation("com.google.android.material:material:1.12.0")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.2.1")
