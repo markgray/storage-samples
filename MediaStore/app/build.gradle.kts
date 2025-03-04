a4cdc9df27c6831056acc6a99c7575251dc14a33
Mark Gray <mark.t.gray@gmail.com>
2025-03-04 04:19:12 -0500

    Updated AGP to 8.8.2 and updated gradle dependencies in MediaStore

--- MediaStore/app/build.gradle.kts
+++ MediaStore/app/build.gradle.kts
@@ -53,10 +53,10 @@ android {
 }
 
 dependencies {
-    implementation("androidx.activity:activity-ktx:1.10.0")
+    implementation("androidx.activity:activity-ktx:1.10.1")
     implementation("androidx.appcompat:appcompat:1.7.0")
     implementation("com.google.android.material:material:1.12.0")
-    implementation("androidx.constraintlayout:constraintlayout:2.2.0")
+    implementation("androidx.constraintlayout:constraintlayout:2.2.1")
     implementation("androidx.vectordrawable:vectordrawable:1.2.0")
     implementation("com.github.bumptech.glide:glide:4.16.0")
 
