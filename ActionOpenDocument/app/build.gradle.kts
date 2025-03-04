40e52d4df1c88e09f5b00d2d1a87ac8e8e424870
Mark Gray <mark.t.gray@gmail.com>
2025-03-04 03:47:53 -0500

    Updated AGP to 8.8.2 and updated gradle dependencies in ActionOpenDocument

--- ActionOpenDocument/app/build.gradle.kts
+++ ActionOpenDocument/app/build.gradle.kts
@@ -49,13 +49,13 @@ android {
 }
 
 dependencies {
-    implementation("androidx.activity:activity-ktx:1.10.0")
+    implementation("androidx.activity:activity-ktx:1.10.1")
     implementation("androidx.annotation:annotation:1.9.1")
     implementation("androidx.appcompat:appcompat:1.7.0")
     implementation("androidx.legacy:legacy-support-v4:1.0.0")
     implementation("androidx.core:core-ktx:1.15.0")
     implementation("androidx.fragment:fragment-ktx:1.8.6")
-    implementation("androidx.constraintlayout:constraintlayout:2.2.0")
+    implementation("androidx.constraintlayout:constraintlayout:2.2.1")
 
     implementation("com.google.android.material:material:1.12.0")
 
