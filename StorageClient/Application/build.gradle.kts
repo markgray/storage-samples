4c922b7115b8029a25de3b31d4f1151a7454d7ba
Mark Gray <mark.t.gray@gmail.com>
2025-04-12 04:53:03 -0400

    Updated gradle dependency in StorageClient

--- StorageClient/Application/build.gradle.kts
+++ StorageClient/Application/build.gradle.kts
@@ -7,7 +7,7 @@ dependencies {
     implementation("androidx.activity:activity-ktx:1.10.1")
     implementation("androidx.cardview:cardview:1.0.0")
     implementation("androidx.appcompat:appcompat:1.7.0")
-    implementation("androidx.core:core-ktx:1.15.0")
+    implementation("androidx.core:core-ktx:1.16.0")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.10.1")
     implementation("androidx.fragment:fragment-ktx:1.8.6")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.8.7")
