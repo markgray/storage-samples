80236642ea90e666591ba7f44fc4e080a663a83c
Mark Gray <mark.t.gray@gmail.com>
2025-05-10 03:36:36 -0400

    Updated AGP to 8.10.0 and updated gradle to 8.14 and updated gradle dependencies in ActionOpenDocumentTree

--- ActionOpenDocumentTree/app/build.gradle.kts
+++ ActionOpenDocumentTree/app/build.gradle.kts
@@ -63,11 +63,11 @@ dependencies {
 
     implementation("com.google.android.material:material:1.12.0")
 
-    implementation("androidx.documentfile:documentfile:1.0.1")
+    implementation("androidx.documentfile:documentfile:1.1.0")
 
     implementation("androidx.core:core-ktx:1.16.0")
     implementation("androidx.fragment:fragment-ktx:1.8.6")
-    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.8.7")
+    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.9.0")
 
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test:runner:1.6.2")
