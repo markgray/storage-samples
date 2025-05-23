e00c1a513a1896cf4bb4b672dfc18b3f01ae5a5c
Mark Gray <mark.t.gray@gmail.com>
2025-05-23 04:35:02 -0400

    Updated kotlin to 2.1.21 and updated gradle dependency in ActionOpenDocument

--- ActionOpenDocument/app/build.gradle.kts
+++ ActionOpenDocument/app/build.gradle.kts
@@ -54,7 +54,7 @@ dependencies {
     implementation("androidx.appcompat:appcompat:1.7.0")
     implementation("androidx.legacy:legacy-support-v4:1.0.0")
     implementation("androidx.core:core-ktx:1.16.0")
-    implementation("androidx.fragment:fragment-ktx:1.8.6")
+    implementation("androidx.fragment:fragment-ktx:1.8.7")
     implementation("androidx.constraintlayout:constraintlayout:2.2.1")
 
     implementation("com.google.android.material:material:1.12.0")
