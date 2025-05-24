7a8d0cfee7151770d10f97b46e158b14dd74a263
Mark Gray <mark.t.gray@gmail.com>
2025-05-24 04:01:31 -0400

    Updated kotlin to 2.1.21 and updated gradle dependency in ScopedStorage

--- ScopedStorage/app/build.gradle.kts
+++ ScopedStorage/app/build.gradle.kts
@@ -59,7 +59,7 @@ dependencies {
     implementation("androidx.appcompat:appcompat:1.7.0")
     implementation("com.google.android.material:material:1.12.0")
     implementation("androidx.constraintlayout:constraintlayout:2.2.1")
-    implementation("androidx.fragment:fragment-ktx:1.8.6")
+    implementation("androidx.fragment:fragment-ktx:1.8.7")
     implementation("androidx.navigation:navigation-fragment-ktx:2.9.0")
     implementation("androidx.navigation:navigation-ui-ktx:2.9.0")
     implementation("androidx.recyclerview:recyclerview:1.4.0")
