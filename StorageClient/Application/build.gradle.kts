c71560fb3d70476f536d4603808d08f90f513e22
markgray <mark.t.gray@gmail.com>
2025-06-06 05:55:47 -0400

    Updated AGP to 8.10.1, and updated gradle to 8.14.1 and updated gradle dependencies in StorageClient

--- StorageClient/Application/build.gradle.kts
+++ StorageClient/Application/build.gradle.kts
@@ -6,11 +6,11 @@ plugins {
 dependencies {
     implementation("androidx.activity:activity-ktx:1.10.1")
     implementation("androidx.cardview:cardview:1.0.0")
-    implementation("androidx.appcompat:appcompat:1.7.0")
+    implementation("androidx.appcompat:appcompat:1.7.1")
     implementation("androidx.core:core-ktx:1.16.0")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.10.2")
-    implementation("androidx.fragment:fragment-ktx:1.8.7")
-    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.9.0")
+    implementation("androidx.fragment:fragment-ktx:1.8.8")
+    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.9.1")
 }
 
 android {
