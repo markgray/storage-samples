38a6f1093fc9acf82615314d4c179ac59911a80b
Mark Gray <mark.t.gray@gmail.com>
2025-08-03 04:15:03 -0400

    Updated AGP to 8.12.0, updated gradle to 9.0.0, and updated gradle dependencies in ActionOpenDocument

--- ActionOpenDocument/app/build.gradle.kts
+++ ActionOpenDocument/app/build.gradle.kts
@@ -48,11 +48,11 @@ dependencies {
     implementation("com.google.android.material:material:1.12.0")
 
     // Testing dependencies
-    androidTestImplementation("androidx.test.espresso:espresso-contrib:3.6.1")
-    androidTestImplementation("androidx.test.espresso:espresso-core:3.6.1")
+    androidTestImplementation("androidx.test.espresso:espresso-contrib:3.7.0")
+    androidTestImplementation("androidx.test.espresso:espresso-core:3.7.0")
     androidTestImplementation("androidx.annotation:annotation:1.9.1")
-    androidTestImplementation("androidx.test:runner:1.6.2")
-    androidTestImplementation("androidx.test:rules:1.6.1")
-    androidTestImplementation("androidx.test.ext:junit:1.2.1")
+    androidTestImplementation("androidx.test:runner:1.7.0")
+    androidTestImplementation("androidx.test:rules:1.7.0")
+    androidTestImplementation("androidx.test.ext:junit:1.3.0")
     testImplementation("junit:junit:4.13.2")
 }
