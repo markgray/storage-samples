62814bbb2e1929eb6d3c65d6395a49fb5dcf6496
markgray <mark.t.gray@gmail.com>
2025-08-03 07:17:54 -0400

    Updated AGP to 8.12.0, updated gradle to 9.0.0, and updated gradle dependencies in FileProvider

--- FileProvider/app/build.gradle.kts
+++ FileProvider/app/build.gradle.kts
@@ -50,15 +50,15 @@ dependencies {
     implementation("androidx.appcompat:appcompat:1.7.1")
     implementation("androidx.constraintlayout:constraintlayout:2.2.1")
     implementation("androidx.fragment:fragment-ktx:1.8.8")
-    implementation("androidx.navigation:navigation-fragment-ktx:2.9.2")
-    implementation("androidx.navigation:navigation-ui-ktx:2.9.2")
+    implementation("androidx.navigation:navigation-fragment-ktx:2.9.3")
+    implementation("androidx.navigation:navigation-ui-ktx:2.9.3")
     implementation("androidx.lifecycle:lifecycle-extensions:2.2.0")
     implementation("com.google.android.material:material:1.12.0")
     implementation("com.squareup.okhttp3:okhttp:5.1.0")
 
     testImplementation("junit:junit:4.13.2")
-    androidTestImplementation("androidx.test.ext:junit:1.2.1")
-    androidTestImplementation("androidx.test.espresso:espresso-core:3.6.1")
+    androidTestImplementation("androidx.test.ext:junit:1.3.0")
+    androidTestImplementation("androidx.test.espresso:espresso-core:3.7.0")
 
     // TODO: coil 2.1.0 breaks our code -- need to migrate
     //noinspection NewerVersionAvailable,GradleDependency
