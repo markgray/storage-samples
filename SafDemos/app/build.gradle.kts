862a5d0ad5cc3acd6af502ea023237bde13fc2a0
Mark Gray <mark.t.gray@gmail.com>
2025-08-16 03:39:23 -0400

    Updated kotlin to 2.2.10, fixed warnings, and updated gradle dependencies in SafDemos

--- SafDemos/app/build.gradle.kts
+++ SafDemos/app/build.gradle.kts
@@ -42,7 +42,7 @@ android {
 dependencies {
     implementation("androidx.activity:activity-ktx:1.10.1")
     implementation("androidx.appcompat:appcompat:1.7.1")
-    implementation("androidx.core:core-ktx:1.16.0")
+    implementation("androidx.core:core-ktx:1.17.0")
     implementation("androidx.constraintlayout:constraintlayout:2.2.1")
     implementation("androidx.navigation:navigation-fragment-ktx:2.9.3")
     implementation("androidx.navigation:navigation-ui-ktx:2.9.3")
