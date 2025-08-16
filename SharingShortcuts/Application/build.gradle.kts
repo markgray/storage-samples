6c8374e38fb066956a04fe24bbd329fd8ea4445d
Mark Gray <mark.t.gray@gmail.com>
2025-08-16 03:54:21 -0400

    Updated kotlin to 2.2.10, fixed warnings, and updated gradle dependencies in SharingShortcuts

--- SharingShortcuts/Application/build.gradle.kts
+++ SharingShortcuts/Application/build.gradle.kts
@@ -10,7 +10,7 @@ dependencies {
     implementation("androidx.appcompat:appcompat:1.7.1")
     implementation("androidx.sharetarget:sharetarget:1.2.0")
     implementation("androidx.recyclerview:recyclerview:1.4.0")
-    implementation("androidx.core:core-ktx:1.16.0")
+    implementation("androidx.core:core-ktx:1.17.0")
 }
 
 android {
