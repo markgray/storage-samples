4605d8452a78da7bfd592d28cebe314e42af9f62
Mark Gray <mark.t.gray@gmail.com>
2025-08-16 03:43:47 -0400

    Updated kotlin to 2.2.10, fixed warnings, and updated gradle dependencies in ScopedStorage

--- ScopedStorage/app/build.gradle.kts
+++ ScopedStorage/app/build.gradle.kts
@@ -43,11 +43,11 @@ android {
 dependencies {
     implementation("androidx.activity:activity-ktx:1.10.1")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.10.2")
-    implementation("androidx.core:core-ktx:1.16.0")
+    implementation("androidx.core:core-ktx:1.17.0")
     implementation("androidx.appcompat:appcompat:1.7.1")
     implementation("com.google.android.material:material:1.12.0")
     implementation("androidx.constraintlayout:constraintlayout:2.2.1")
-    implementation("androidx.fragment:fragment-ktx:1.8.8")
+    implementation("androidx.fragment:fragment-ktx:1.8.9")
     implementation("androidx.navigation:navigation-fragment-ktx:2.9.3")
     implementation("androidx.navigation:navigation-ui-ktx:2.9.3")
     implementation("androidx.recyclerview:recyclerview:1.4.0")
