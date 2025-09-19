b3915ceb5559234dec6c982ecf5eb06759f5be42
Mark Gray <mark.t.gray@gmail.com>
2025-09-19 03:44:53 -0400

    Updated AGP to 8.13.0, updated kotlin to 2.2.20, fixed warnings, and updated gradle dependencies in ActionOpenDocument

--- ActionOpenDocument/app/build.gradle.kts
+++ ActionOpenDocument/app/build.gradle.kts
@@ -37,7 +37,7 @@ android {
 }
 
 dependencies {
-    implementation("androidx.activity:activity-ktx:1.10.1")
+    implementation("androidx.activity:activity-ktx:1.11.0")
     implementation("androidx.annotation:annotation:1.9.1")
     implementation("androidx.appcompat:appcompat:1.7.1")
     implementation("androidx.legacy:legacy-support-v4:1.0.0")
@@ -45,7 +45,7 @@ dependencies {
     implementation("androidx.fragment:fragment-ktx:1.8.9")
     implementation("androidx.constraintlayout:constraintlayout:2.2.1")
 
-    implementation("com.google.android.material:material:1.12.0")
+    implementation("com.google.android.material:material:1.13.0")
 
     // Testing dependencies
     androidTestImplementation("androidx.test.espresso:espresso-contrib:3.7.0")
