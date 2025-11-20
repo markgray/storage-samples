d8a6859fb67a44672c48a77fdf07ccaf7f8579aa
Mark Gray <mark.t.gray@gmail.com>
2025-11-20 01:23:29 -0500

    Updated AGP to 8.13.1, and updated gradle dependencies and increased minSdk to 23 in MediaStore

--- MediaStore/app/build.gradle.kts
+++ MediaStore/app/build.gradle.kts
@@ -20,7 +20,7 @@ android {
     }
     defaultConfig {
         applicationId = "com.android.samples.mediastore"
-        minSdk = 21
+        minSdk = 23
         targetSdk = 36
         versionCode = 1
         versionName = "1.0"
@@ -42,7 +42,7 @@ android {
 }
 
 dependencies {
-    implementation("androidx.activity:activity-ktx:1.11.0")
+    implementation("androidx.activity:activity-ktx:1.12.0")
     implementation("androidx.appcompat:appcompat:1.7.1")
     implementation("com.google.android.material:material:1.13.0")
     implementation("androidx.constraintlayout:constraintlayout:2.2.1")
