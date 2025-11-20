d36cb73e3cf33c9a8b53ce7e19880a5aa5b87508
Mark Gray <mark.t.gray@gmail.com>
2025-11-20 03:47:50 -0500

    Updated AGP to 8.13.1, and updated gradle dependencies and increased minSdk to 23 in StorageClient

--- StorageClient/Application/build.gradle.kts
+++ StorageClient/Application/build.gradle.kts
@@ -6,19 +6,19 @@ plugins {
 }
 
 dependencies {
-    implementation("androidx.activity:activity-ktx:1.11.0")
+    implementation("androidx.activity:activity-ktx:1.12.0")
     implementation("androidx.cardview:cardview:1.0.0")
     implementation("androidx.appcompat:appcompat:1.7.1")
     implementation("androidx.core:core-ktx:1.17.0")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.10.2")
     implementation("androidx.fragment:fragment-ktx:1.8.9")
-    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.9.4")
+    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.10.0")
 }
 
 android {
     compileSdk = 36
     defaultConfig {
-        minSdk = 21
+        minSdk = 23
         targetSdk = 36
     }
     compileOptions {
