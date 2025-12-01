b6eef7ede45dd55cf74b4e96ccaa7cc965b59565
markgray <mark.t.gray@gmail.com>
2025-12-01 06:19:51 -0500

    Added comments in SafDemos

--- SafDemos/app/src/main/java/com/android/samples/safdemos/MainActivity.kt
+++ SafDemos/app/src/main/java/com/android/samples/safdemos/MainActivity.kt
@@ -18,8 +18,10 @@ package com.android.samples.safdemos
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.view.ViewGroup
 import androidx.activity.enableEdgeToEdge
+import androidx.core.graphics.Insets
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import androidx.core.view.updateLayoutParams
@@ -38,16 +40,29 @@ import com.android.samples.safdemos.imagepicker.ImagePickerFragment
 class MainActivity : AppCompatActivity() {
 
     /**
-     * Called when the activity is starting. First we call our super's implementation of `onCreate`.
-     * Then we set our [ActivityMainBinding] variable `val binding` to the instance that the
-     * [ActivityMainBinding.inflate] method inflates from its associated layout file activity_main.xml
-     * (resource ID `R.layout.activity_main`). This layout consists of a vertical `LinearLayout` root
-     * view holding an `AppBarLayout` (which holds a `Toolbar`) above a `FragmentContainerView` whose
-     * fragment is the `NavHostFragment` for the navigation graph navigation/nav_graph.xml of the app.
+     * Called when the activity is starting. First we call [enableEdgeToEdge] to enable edge to edge
+     * display, then we call our super's implementation of `onCreate`, and set our
+     * [ActivityMainBinding] variable `val binding` to the instance that the
+     * [ActivityMainBinding.inflate] method inflates from its associated layout file
+     * `activity_main.xml` (resource ID `R.layout.activity_main`). This layout consists of a
+     * vertical `LinearLayout` root view holding an `AppBarLayout` (which holds a `Toolbar`) above
+     * a `FragmentContainerView` whose fragment is the `NavHostFragment` for the navigation graph
+     * navigation/nav_graph.xml of the app.
      *
-     * Next we set our content view to the outermost View in the associated layout file of `binding`,
-     * and set the [ActivityMainBinding.toolbar] in `binding` to act as the ActionBar for this
-     * Activity window.
+     * Next we set our content view to [ActivityMainBinding.getRoot], the outermost View in the
+     * associated layout file of `binding`, and set the [ActivityMainBinding.toolbar] in `binding`
+     * to act as the ActionBar for this Activity window.
+     *
+     * Next we call the [ViewCompat.setOnApplyWindowInsetsListener] method to take over the policy
+     * for applying window insets to the [ActivityMainBinding.getRoot] root view of `binding`, with
+     * the `listener` argument a lambda that accepts the [View] passed the lambda in variable `v`
+     * and the [WindowInsetsCompat] passed the lambda in variable `windowInsets`. It initializes its
+     * [Insets] variable `insets` to the [WindowInsetsCompat.getInsets] of `windowInsets` with
+     * [WindowInsetsCompat.Type.systemBars] as the argument, then it updates the layout parameters
+     * of `v` to be a [ViewGroup.MarginLayoutParams] with the left margin set to `insets.left`, the
+     * right margin set to `insets.right`, the top margin set to `insets.top`, and the bottom margin
+     * set to `insets.bottom`. Finally it returns [WindowInsetsCompat.CONSUMED] to the caller (so
+     * that the window insets will not keep passing down to descendant views).
      *
      * @param savedInstanceState we do not override [onSaveInstanceState] so do not use this.
      */
@@ -56,9 +71,10 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         val binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        val rootView = binding.root
-        ViewCompat.setOnApplyWindowInsetsListener(rootView) { v, windowInsets ->
-            val insets = windowInsets.getInsets(WindowInsetsCompat.Type.systemBars())
+        setSupportActionBar(binding.toolbar)
+
+        ViewCompat.setOnApplyWindowInsetsListener(binding.root) { v: View, windowInsets: WindowInsetsCompat ->
+            val insets: Insets = windowInsets.getInsets(WindowInsetsCompat.Type.systemBars())
             // Apply the insets as a margin to the view.
             v.updateLayoutParams<ViewGroup.MarginLayoutParams> {
                 leftMargin = insets.left
@@ -71,6 +87,5 @@ class MainActivity : AppCompatActivity() {
             WindowInsetsCompat.CONSUMED
         }
 
-        setSupportActionBar(binding.toolbar)
     }
 }
