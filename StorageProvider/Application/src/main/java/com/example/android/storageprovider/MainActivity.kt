7b5ecc5f1d848f1721ad2d48e17bb6206839083e
markgray <mark.t.gray@gmail.com>
2025-12-01 06:59:16 -0500

    Added comments in StorageProvider

--- StorageProvider/Application/src/main/java/com/example/android/storageprovider/MainActivity.kt
+++ StorageProvider/Application/src/main/java/com/example/android/storageprovider/MainActivity.kt
@@ -22,13 +22,17 @@ import android.os.Bundle
 import android.view.Menu
 import android.view.MenuInflater
 import android.view.MenuItem
+import android.view.View
 import android.view.ViewGroup
 import android.widget.LinearLayout
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
+import androidx.core.graphics.Insets
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import androidx.core.view.updateLayoutParams
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentContainerView
 import androidx.fragment.app.FragmentManager
 import androidx.fragment.app.FragmentTransaction
 import com.example.android.common.activities.SampleActivityBase
@@ -44,15 +48,28 @@ import com.example.android.common.logger.MessageOnlyLogFilter
  */
 class MainActivity : SampleActivityBase() {
     /**
-     * Called when the activity is starting. First we call our super's implementation of `onCreate`,
-     * then we set our content view to our layout file `R.layout.activity_main`, which consists of
-     * a vertical `LinearLayout` root view that holds a `TextView` displaying our info message, with
-     * a 1dp "darker_gray" `View` seperating it from a `FragmentContainerView` that holds a
-     * `LogFragment` that displays any messages our app logs.
+     * Called when the activity is starting. First we call [enableEdgeToEdge] to enable edge to edge
+     * display, then we call our super's implementation of `onCreate`, and set our content view to
+     * our layout file `R.layout.activity_main`, which consists of a vertical [LinearLayout] root
+     * view that holds a [TextView] displaying our info message, with a 1dp "darker_gray" [View]
+     * seperating it from a [FragmentContainerView] that holds a [LogFragment] that displays any
+     * messages our app logs.
+     *
+     * We initialize our [LinearLayout] variable `rootView` to the view with ID
+     * `R.id.sample_main_layout` then call [ViewCompat.setOnApplyWindowInsetsListener]
+     * to take over the policy for applying window insets to `rootView`, with the `listener`
+     * argument a lambda that accepts the [View] passed the lambda in variable `v` and the
+     * [WindowInsetsCompat] passed the lambda in variable `windowInsets`. It initializes its
+     * [Insets] variable `insets` to the [WindowInsetsCompat.getInsets] of `windowInsets` with
+     * [WindowInsetsCompat.Type.systemBars] as the argument, then it updates the layout parameters
+     * of `v` to be a [ViewGroup.MarginLayoutParams] with the left margin set to `insets.left`,
+     * the right margin set to `insets.right`, the top margin set to `insets.top`, and the bottom
+     * margin set to `insets.bottom`. Finally it returns [WindowInsetsCompat.CONSUMED] to the caller
+     * (so that the window insets will not keep passing down to descendant views).
      *
      * If the [FragmentManager] for interacting with fragments associated with this activity cannot
-     * find a [Fragment] whose tag is [FRAGTAG] (the [String] "StorageProviderFragment") we initialize
-     * our [FragmentTransaction] variable `val transaction` to the instance that the
+     * find a [Fragment] whose tag is [FRAGTAG] (the [String] "StorageProviderFragment") we
+     * initialize our [FragmentTransaction] variable `val transaction` to the instance that the
      * [FragmentManager.beginTransaction] method of the [FragmentManager] for interacting with
      * fragments associated with this activity returns, initialize our [StorageProviderFragment]
      * variable `val fragment` to a new instance, use `transaction` to add `fragment` with the tag
@@ -65,8 +82,8 @@ class MainActivity : SampleActivityBase() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         val rootView = findViewById<LinearLayout>(R.id.sample_main_layout)
-        ViewCompat.setOnApplyWindowInsetsListener(rootView) { v, windowInsets ->
-            val insets = windowInsets.getInsets(WindowInsetsCompat.Type.systemBars())
+        ViewCompat.setOnApplyWindowInsetsListener(rootView) { v: View, windowInsets: WindowInsetsCompat ->
+            val insets: Insets = windowInsets.getInsets(WindowInsetsCompat.Type.systemBars())
             // Apply the insets as a margin to the view.
             v.updateLayoutParams<ViewGroup.MarginLayoutParams> {
                 leftMargin = insets.left
