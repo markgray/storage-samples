648b58d5dd15255ab90d98427314eb0d0ff86a04
markgray <mark.t.gray@gmail.com>
2025-12-01 06:29:54 -0500

    Added comments in ScopedStorage

--- ScopedStorage/app/src/main/java/com/samples/storage/MainActivity.kt
+++ ScopedStorage/app/src/main/java/com/samples/storage/MainActivity.kt
@@ -16,10 +16,12 @@
 package com.samples.storage
 
 import android.os.Bundle
+import android.view.View
 import android.view.ViewGroup
 import androidx.appcompat.app.AppCompatActivity
 import androidx.activity.enableEdgeToEdge
 import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.core.graphics.Insets
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import androidx.core.view.updateLayoutParams
@@ -49,19 +51,34 @@ class MainActivity : AppCompatActivity() {
     private lateinit var appBarConfiguration: AppBarConfiguration
 
     /**
-     * Called then the activity is starting. First we call our super's implementation of `onCreate`,
-     * then we set our content view to our layout file `R.layout.main_activity`. It consists of a
-     * `ConstraintLayout` root view holding a single `fragment` container whose "android:name"
-     * attribute specifies that a [NavHostFragment] will be instantiated in it. We initialize our
-     * [NavController] variable `val navController` by finding the [NavHostFragment] with the
-     * resource ID `R.id.nav_host_fragment` in our UI. We initialize our [AppBarConfiguration] field
-     * [appBarConfiguration] to a new instance constructed to use the navigation graph of
-     * `navController` (the file navigation/nav_graph.xml) for its "UP" behavior (its start
-     * destination ([MainFragment]) will be considered the only top level destination, and the "Up"
-     * button will not be displayed when on the start destination of the graph). Finally we call the
-     * [setupActionBarWithNavController] method with `navController` as the [NavController] whose
-     * navigation actions will be reflected in the title of the action bar, and [appBarConfiguration]
-     * for additional configuration options for customizing the behavior of the `ActionBar`.
+     * Called when the activity is starting. First we call [enableEdgeToEdge] to enable edge to edge
+     * display, then we call our super's implementation of `onCreate`, and set our content view to
+     * our layout file `R.layout.activity_main`. It consists of a [ConstraintLayout] root view
+     * holding a single `fragment` container whose "android:name" attribute specifies that a
+     * [NavHostFragment] will be instantiated in it.
+     *
+     * We initialize our [ConstraintLayout] variable `rootView` to the view with ID `R.id.root_view`
+     * then call [ViewCompat.setOnApplyWindowInsetsListener] to take over the policy for applying
+     * window insets to `rootView`, with the `listener` argument a lambda that accepts the [View]
+     * passed the lambda in variable `v` and the [WindowInsetsCompat] passed the lambda in variable
+     * `windowInsets`. It initializes its [Insets] variable `insets` to the
+     * [WindowInsetsCompat.getInsets] of `windowInsets` with [WindowInsetsCompat.Type.systemBars]
+     * as the argument, then it updates the layout parameters of `v` to be a
+     * [ViewGroup.MarginLayoutParams] with the left margin set to `insets.left`, the right margin
+     * set to `insets.right`, the top margin set to `insets.top`, and the bottom margin set to
+     * `insets.bottom`. Finally it returns [WindowInsetsCompat.CONSUMED] to the caller (so that
+     * the window insets will not keep passing down to descendant views).
+     *
+     * We initialize our [NavController] variable `val navController` by finding the
+     * [NavHostFragment] with the resource ID `R.id.nav_host_fragment` in our UI. We initialize our
+     * [AppBarConfiguration] field [appBarConfiguration] to a new instance constructed to use the
+     * navigation graph of `navController` (the file navigation/nav_graph.xml) for its "UP" behavior
+     * (its start destination ([MainFragment]) will be considered the only top level destination,
+     * and the "Up" button will not be displayed when on the start destination of the graph).
+     * Finally we call the [setupActionBarWithNavController] method with `navController` as the
+     * [NavController] whose navigation actions will be reflected in the title of the action bar,
+     * and [appBarConfiguration] for additional configuration options for customizing the behavior
+     * of the `ActionBar`.
      *
      * @param savedInstanceState we do not override [onSaveInstanceState] so do not use.
      */
@@ -70,8 +87,8 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.main_activity)
         val rootView = findViewById<ConstraintLayout>(R.id.root_view)
-        ViewCompat.setOnApplyWindowInsetsListener(rootView) { v, windowInsets ->
-            val insets = windowInsets.getInsets(WindowInsetsCompat.Type.systemBars())
+        ViewCompat.setOnApplyWindowInsetsListener(rootView) { v: View, windowInsets: WindowInsetsCompat ->
+            val insets: Insets = windowInsets.getInsets(WindowInsetsCompat.Type.systemBars())
             // Apply the insets as a margin to the view.
             v.updateLayoutParams<ViewGroup.MarginLayoutParams> {
                 leftMargin = insets.left
