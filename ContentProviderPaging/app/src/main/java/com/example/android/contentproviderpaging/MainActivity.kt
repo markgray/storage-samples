fc06de6809d57e337b3a4ebd73e3f735d926d497
markgray <mark.t.gray@gmail.com>
2025-12-01 03:53:24 -0500

    Added comments in ContentProviderPaging

--- ContentProviderPaging/app/src/main/java/com/example/android/contentproviderpaging/MainActivity.kt
+++ ContentProviderPaging/app/src/main/java/com/example/android/contentproviderpaging/MainActivity.kt
@@ -16,10 +16,12 @@
 package com.example.android.contentproviderpaging
 
 import android.os.Bundle
+import android.view.View
 import android.view.ViewGroup
 import android.widget.FrameLayout
 import androidx.appcompat.app.AppCompatActivity
 import androidx.activity.enableEdgeToEdge
+import androidx.core.graphics.Insets
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import androidx.core.view.updateLayoutParams
@@ -32,15 +34,30 @@ import com.example.android.contentproviderpaging.common.R
  */
 class MainActivity : AppCompatActivity() {
     /**
-     * Called when the activity is starting. First we call our super's implementation of `onCreate`,
-     * then we set our content view to our layout file `R.layout.activity_main` which consists of a
-     * single `FrameLayout` root view with the resource ID `R.id.container`. If our [Bundle] parameter
-     * [savedInstanceState] is `null` this is the first time we are being run so we fetch the
-     * [FragmentManager] for interacting with fragments associated with this activity and use it to
-     * begin a [FragmentTransaction] which we use to add a new instance of [ImageClientFragment] to
-     * the container with resource ID `R.id.container` and then we commit the [FragmentTransaction].
-     * Note: if [savedInstanceState] is not `null` we are being restarted after previously being
-     * shut down and the system will take care of restoring our [ImageClientFragment] for us.
+     * Called when the activity is starting. First we call [enableEdgeToEdge] to enable edge to
+     * edge display, then we call our super's implementation of `onCreate`, and set our content
+     * view to our layout file `R.layout.activity_main` which consists of a single [FrameLayout]
+     * root view with the resource ID `R.id.container`.
+     *
+     * We initialize our [FrameLayout] variable `rootView` to the view with ID `R.id.container`
+     * then call [ViewCompat.setOnApplyWindowInsetsListener] to take over the policy for applying
+     * window insets to `rootView`, with the `listener` argument a lambda that accepts the [View]
+     * passed the lambda in variable `v` and the [WindowInsetsCompat] passed the lambda in variable
+     * `windowInsets`. It initializes its [Insets] variable `insets` to the
+     * [WindowInsetsCompat.getInsets] of `windowInsets` with [WindowInsetsCompat.Type.systemBars]
+     * as the argument, then it updates the layout parameters of `v` to be a
+     * [ViewGroup.MarginLayoutParams] with the left margin set to `insets.left`, the right margin
+     * set to `insets.right`, the top margin set to `insets.top`, and the bottom margin set to
+     * `insets.bottom`. Finally it returns [WindowInsetsCompat.CONSUMED] to the caller (so that the
+     * window insets will not keep passing down to descendant views).
+     *
+     * If our [Bundle] parameter [savedInstanceState] is `null` this is the first time we are being
+     * called so we fetch the [FragmentManager] for interacting with fragments associated with this
+     * activity and use it to begin a [FragmentTransaction] which we use to add a new instance of
+     * [ImageClientFragment] to the container with resource ID `R.id.container` and then we commit
+     * the [FragmentTransaction]. Note: if [savedInstanceState] is not `null` we are being restarted
+     * after previously being shut down and the system will take care of restoring our
+     * [ImageClientFragment] fragment for us.
      *
      * @param savedInstanceState If the activity is being re-initialized after previously being shut
      * down then this [Bundle] contains the data it most recently supplied in [onSaveInstanceState].
@@ -51,8 +68,8 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         val rootView = findViewById<FrameLayout>(R.id.container)
-        ViewCompat.setOnApplyWindowInsetsListener(rootView) { v, windowInsets ->
-            val insets = windowInsets.getInsets(WindowInsetsCompat.Type.systemBars())
+        ViewCompat.setOnApplyWindowInsetsListener(rootView) { v: View, windowInsets: WindowInsetsCompat ->
+            val insets: Insets = windowInsets.getInsets(WindowInsetsCompat.Type.systemBars())
             // Apply the insets as a margin to the view.
             v.updateLayoutParams<ViewGroup.MarginLayoutParams> {
                 leftMargin = insets.left
