1a2fa41a0adbbbd141ca0cfd88ef16612d3143a2
markgray <mark.t.gray@gmail.com>
2025-12-01 06:51:24 -0500

    Added comments in StorageClient

--- StorageClient/Application/src/main/java/com/example/android/storageclient/MainActivity.kt
+++ StorageClient/Application/src/main/java/com/example/android/storageclient/MainActivity.kt
@@ -21,15 +21,20 @@ import android.os.Bundle
 import android.view.Menu
 import android.view.MenuInflater
 import android.view.MenuItem
+import android.view.View
 import android.view.ViewGroup
 import android.widget.LinearLayout
+import android.widget.ScrollView
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
+import androidx.core.graphics.Insets
 import androidx.core.view.MenuHost
 import androidx.core.view.MenuProvider
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import androidx.core.view.updateLayoutParams
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentContainerView
 import androidx.fragment.app.FragmentManager
 import androidx.fragment.app.FragmentTransaction
 import com.example.android.common.activities.SampleActivityBase
@@ -45,11 +50,31 @@ import com.example.android.common.logger.MessageOnlyLogFilter
  */
 class MainActivity : SampleActivityBase() {
     /**
-     * Called when the activity is starting. First we call our super's implementation of `onCreate`,
-     * then we set our content view to our layout file `R.layout.activity_main`, which consists of
-     * a vertical `LinearLayout` root view that holds a `ScrollView` holding a `TextView` displaying
-     * our into message, with a 1dp "darker_gray" `View` seperating it from a `FragmentContainerView`
-     * that holds a `LogFragment` that displays any messages our app logs.
+     * Called when the activity is starting. First we call [enableEdgeToEdge] to enable edge to edge
+     * display, then we call our super's implementation of `onCreate`, and set our content view to
+     * our layout file `R.layout.activity_main`, which consists of a vertical [LinearLayout] root
+     * view that holds a [ScrollView] holding a [TextView] displaying our into message, with a 1dp
+     * "darker_gray" [View] seperating it from a [FragmentContainerView] that holds a [LogFragment]
+     * that displays any messages our app logs.
+     *
+     * We initialize our [LinearLayout] variable `rootView` to the view with ID
+     * `R.id.sample_main_layout` then call [ViewCompat.setOnApplyWindowInsetsListener]
+     * to take over the policy for applying window insets to `rootView`, with the
+     * `listener` argument a lambda that accepts the [View] passed the lambda in
+     * variable `v` and the [WindowInsetsCompat] passed the lambda in variable
+     * `windowInsets`. It initializes its [Insets] variable `insets` to the
+     * [WindowInsetsCompat.getInsets] of `windowInsets` with
+     * [WindowInsetsCompat.Type.systemBars] as the argument, then it updates
+     * the layout parameters of `v` to be a [ViewGroup.MarginLayoutParams]
+     * with the left margin set to `insets.left`, the right margin set to
+     * `insets.right`, the top margin set to `insets.top`, and the bottom margin
+     * set to `insets.bottom`. Finally it returns [WindowInsetsCompat.CONSUMED]
+     * to the caller (so that the window insets will not keep passing down to
+     * descendant views).
+     *
+     * We initialize our [MenuHost] variable `menuHost` to `this` activity, then call
+     * [MenuHost.addMenuProvider] with our [MenuProvider] property [menuProvider] as its `provider`
+     * argument and `this` as its `owner` argument.
      *
      * If the [FragmentManager] for interacting with fragments associated with this activity cannot
      * find a [Fragment] whose tag is [FRAGTAG] (the [String] "StorageClientFragment") we initialize
@@ -66,8 +91,8 @@ class MainActivity : SampleActivityBase() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         val rootView = findViewById<LinearLayout>(R.id.sample_main_layout)
-        ViewCompat.setOnApplyWindowInsetsListener(rootView) { v, windowInsets ->
-            val insets = windowInsets.getInsets(WindowInsetsCompat.Type.systemBars())
+        ViewCompat.setOnApplyWindowInsetsListener(rootView) { v: View, windowInsets: WindowInsetsCompat ->
+            val insets: Insets = windowInsets.getInsets(WindowInsetsCompat.Type.systemBars())
             // Apply the insets as a margin to the view.
             v.updateLayoutParams<ViewGroup.MarginLayoutParams> {
                 leftMargin = insets.left
@@ -80,7 +105,7 @@ class MainActivity : SampleActivityBase() {
             WindowInsetsCompat.CONSUMED
         }
         val menuHost: MenuHost = this
-        menuHost.addMenuProvider(menuProvider, this)
+        menuHost.addMenuProvider(/* provider = */ menuProvider, /* owner = */ this)
         if (supportFragmentManager.findFragmentByTag(FRAGTAG) == null) {
             val transaction: FragmentTransaction = supportFragmentManager.beginTransaction()
             val fragment = StorageClientFragment()
@@ -135,6 +160,7 @@ class MainActivity : SampleActivityBase() {
         val logFragment = supportFragmentManager
             .findFragmentById(R.id.log_fragment) as LogFragment?
         msgFilter.next = (logFragment ?: return).logView
+        @SuppressLint("ObsoleteSdkInt") // Kept in for reuse reference
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
             (logFragment.logView ?: return).setTextAppearance(R.style.Log)
         } else {
