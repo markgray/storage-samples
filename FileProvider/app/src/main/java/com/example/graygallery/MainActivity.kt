aac1ed0b2bedceb9a450559973281eaf9b7c29ad
markgray <mark.t.gray@gmail.com>
2025-12-01 04:44:52 -0500

    Added comments in FileProvider

--- FileProvider/app/src/main/java/com/example/graygallery/MainActivity.kt
+++ FileProvider/app/src/main/java/com/example/graygallery/MainActivity.kt
@@ -17,10 +17,12 @@
 package com.example.graygallery
 
 import android.os.Bundle
+import android.view.View
 import android.view.ViewGroup
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.core.graphics.Insets
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import androidx.core.view.updateLayoutParams
@@ -40,10 +42,23 @@ import com.google.android.material.bottomnavigation.BottomNavigationView
  */
 class MainActivity : AppCompatActivity() {
     /**
-     * Called when the activity is starting. First we call our super's implementation of `onCreate`,
-     * then we set our content view to the layout file `R.layout.activity_main`. This consists of a
-     * `ConstraintLayout` root view holding a [BottomNavigationView] at its bottom (resource ID
-     * `R.id.nav_view`) with a `fragment` container at its top (resource ID `R.id.nav_host_fragment`).
+     * Called when the activity is starting. First we call [enableEdgeToEdge] to enable edge to
+     * edge display, then we call our super's implementation of `onCreate`, and set our content
+     * view to our layout file `R.layout.activity_main`. This consists of a [ConstraintLayout]
+     * root view holding a [BottomNavigationView] at its bottom (resource ID `R.id.nav_view`) with
+     * a `fragment` container at its top (resource ID `R.id.nav_host_fragment`).
+     *
+     * We initialize our [ConstraintLayout] variable `rootView` to the view with ID `R.id.container`
+     * then call [ViewCompat.setOnApplyWindowInsetsListener] to take over the policy for applying
+     * window insets to `rootView`, with the `listener` argument a lambda that accepts the [View]
+     * passed the lambda in variable `v` and the [WindowInsetsCompat] passed the lambda in variable
+     * `windowInsets`. It initializes its [Insets] variable `insets` to the
+     * [WindowInsetsCompat.getInsets] of `windowInsets` with [WindowInsetsCompat.Type.systemBars]
+     * as the argument, then it updates the layout parameters of `v` to be a
+     * [ViewGroup.MarginLayoutParams] with the left margin set to `insets.left`, the right margin
+     * set to `insets.right`, the top margin set to `insets.top`, and the bottom margin set to
+     * `insets.bottom`. Finally it returns [WindowInsetsCompat.CONSUMED] to the caller (so that the
+     * window insets will not keep passing down to descendant views).
      *
      * Next we initialize our [BottomNavigationView] variable `val navView` by finding the view with
      * ID `R.id.nav_view`, and our [NavController] variable `val navController` by finding the view
@@ -68,8 +83,8 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         val rootView = findViewById<ConstraintLayout>(R.id.container)
-        ViewCompat.setOnApplyWindowInsetsListener(rootView) { v, windowInsets ->
-            val insets = windowInsets.getInsets(WindowInsetsCompat.Type.systemBars())
+        ViewCompat.setOnApplyWindowInsetsListener(rootView) { v: View, windowInsets: WindowInsetsCompat ->
+            val insets: Insets = windowInsets.getInsets(WindowInsetsCompat.Type.systemBars())
             // Apply the insets as a margin to the view.
             v.updateLayoutParams<ViewGroup.MarginLayoutParams> {
                 leftMargin = insets.left
