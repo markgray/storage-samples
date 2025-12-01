1c55ebb554dd9dc43f6aa004dbe107756084835f
markgray <mark.t.gray@gmail.com>
2025-12-01 06:37:40 -0500

    Added comments in SharingShortcuts

--- SharingShortcuts/Application/src/main/java/com/example/android/sharingshortcuts/MainActivity.kt
+++ SharingShortcuts/Application/src/main/java/com/example/android/sharingshortcuts/MainActivity.kt
@@ -33,6 +33,7 @@ import androidx.activity.enableEdgeToEdge
 import androidx.core.content.FileProvider
 import androidx.core.content.pm.ShortcutInfoCompat
 import androidx.core.content.pm.ShortcutManagerCompat
+import androidx.core.graphics.Insets
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import androidx.core.view.updateLayoutParams
@@ -60,19 +61,34 @@ class MainActivity : ComponentActivity() {
     private lateinit var mSharingShortcutsManager: SharingShortcutsManager
 
     /**
-     * Called when the activity is starting. First we call our super's implementation of `onCreate`,
-     * then we set our content view to our layout file `R.layout.activity_main`. It consists of a
-     * vertical `LinearLayout` holding a [TextView] with the message: "This app demonstrates how to
-     * implement Direct Share. Use some other app and share a text. For your convenience, you can
-     * also use the input below to share the text" above an [EditText] where the user can enter their
-     * message, with a "Share" [Button] at the bottom of the UI. Next we initialize our [EditText]
-     * field [mEditBody] by finding the view with ID `R.id.body`, and set the [View.OnClickListener]
-     * of the view with ID `R.id.share` to our [View.OnClickListener] field [mOnClickListener].
+     * Called when the activity is starting. First we call [enableEdgeToEdge] to enable edge to edge
+     * display, then we call our super's implementation of `onCreate`, and set our content view to
+     * our layout file `R.layout.activity_main`. It consists of a vertical [LinearLayout] holding a
+     * [TextView] with the message: "This app demonstrates how to implement Direct Share. Use some
+     * other app and share a text. For your convenience, you can also use the input below to share
+     * the text" above an [EditText] where the user can enter their message, with a "Share" [Button]
+     * at the bottom of the UI.
+     *
+     * We initialize our [LinearLayout] variable `rootView` to the view with ID `R.id.root_view`
+     * then call [ViewCompat.setOnApplyWindowInsetsListener] to take over the policy for applying
+     * window insets to `rootView`, with the `listener` argument a lambda that accepts the [View]
+     * passed the lambda in variable `v` and the [WindowInsetsCompat] passed the lambda in variable
+     * `windowInsets`. It initializes its [Insets] variable `insets` to the
+     * [WindowInsetsCompat.getInsets] of `windowInsets` with [WindowInsetsCompat.Type.systemBars]
+     * as the argument, then it updates the layout parameters of `v` to be a
+     * [ViewGroup.MarginLayoutParams] with the left margin set to `insets.left`, the right margin
+     * set to `insets.right`, the top margin set to `insets.top`, and the bottom margin set to
+     * `insets.bottom`. Finally it returns [WindowInsetsCompat.CONSUMED] to the caller (so that
+     * the window insets will not keep passing down to descendant views).
+     *
+     * Next we initialize our [EditText] field [mEditBody] by finding the view with ID `R.id.body`,
+     * and set the [View.OnClickListener] of the view with ID `R.id.share` to our
+     * [View.OnClickListener] field [mOnClickListener].
      *
      * Finally we initialize our [SharingShortcutsManager] field [mSharingShortcutsManager] to a new
      * instance and call its [SharingShortcutsManager.pushDirectShareTargets] method to have it
-     * build a [List] of [ShortcutInfoCompat] shortcuts and use the [ShortcutManagerCompat.addDynamicShortcuts]
-     * method to publish that list of dynamic shortcuts.
+     * build a [List] of [ShortcutInfoCompat] shortcuts and use the
+     * [ShortcutManagerCompat.addDynamicShortcuts] method to publish that list of dynamic shortcuts.
      *
      * @param savedInstanceState we do not override [onSaveInstanceState] so do not use.
      */
@@ -81,8 +97,8 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         val rootView = findViewById<LinearLayout>(R.id.root_view)
-        ViewCompat.setOnApplyWindowInsetsListener(rootView) { v, windowInsets ->
-            val insets = windowInsets.getInsets(WindowInsetsCompat.Type.systemBars())
+        ViewCompat.setOnApplyWindowInsetsListener(rootView) { v: View, windowInsets: WindowInsetsCompat ->
+            val insets: Insets = windowInsets.getInsets(WindowInsetsCompat.Type.systemBars())
             // Apply the insets as a margin to the view.
             v.updateLayoutParams<ViewGroup.MarginLayoutParams> {
                 leftMargin = insets.left
